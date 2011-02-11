5f542f93bb07849af05f053faff3efb7eff66124
Thomas Dziedzic <gostrc@gmail.com>
2011-02-11 03:47:31 -0800

    updated on Fri Feb 11 03:47:31 PST 2011

--- thunderbird-beta/run-thunderbird.sh
+++ thunderbird-beta/run-thunderbird.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export NO_EM_RESTART=1
 mkdir $OBJDIR/_profileprofile
-$OBJDIR/dist/bin/thunderbird -no-remote -profile $OBJDIR/_profileprofile
+$OBJDIR/mozilla/dist/bin/thunderbird-bin -no-remote -profile $OBJDIR/_profileprofile
