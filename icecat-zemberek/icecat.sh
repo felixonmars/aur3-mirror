28c59f80ce2a531b677b820da82d3f4404d3b273
Thomas Dziedzic <gostrc@gmail.com>
2011-02-18 08:24:02 -0800

    updated on Fri Feb 18 08:24:02 PST 2011

--- icecat-zemberek/icecat.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-  
-  [ "${ICECATDIR}" = "" ] && export ICECATDIR="/usr/lib/icecat-3.0.1-g1"
-# export LD_LIBRARY_PATH=${ICECATDIR}:${LD_LIBRARY_PATH}
-  export MOZILLA_FIVE_HOME=${ICECATDIR}
-# export MOZ_PLUGIN_PATH=${ICECATDIR}/plugins/:${MOZ_PLUGIN_PATH}
-  export PATH=${PATH}:${ICECATDIR}
