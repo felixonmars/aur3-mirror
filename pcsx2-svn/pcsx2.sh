387e5a1d1eca4e5a024eb75f6674e77b612969e5
Thomas Dziedzic <gostrc@gmail.com>
2011-02-26 19:00:54 -0800

    updated on Sat Feb 26 19:00:54 PST 2011

--- pcsx2-svn/pcsx2.sh
+++ pcsx2-svn/pcsx2.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-# Launch PCSX2 in any build mode
-cd /opt/pcsx2/
-for i in pcsx2-dbg pcsx2-dev pcsx2 fail; do
-	if [ -x $i ]; then
-		./$i $*
-		exit $?
-	fi
-done
-# If did not find, then...
-echo "PCSX2 executable not found" && echo 1
+# Launch PCSX2 emulator
+if [ -x /opt/pcsx2/pcsx2 ]; then
+	cd /opt/pcsx2
+	./pcsx2
+else 
+	echo "PCSX2 executable not found" && exit 1
+fi
 
