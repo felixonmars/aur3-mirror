a611f796e2b6b43aaf5eb66bda2d6cc6e8b0928f
Thomas Dziedzic <gostrc@gmail.com>
2011-03-02 21:00:48 -0800

    updated on Wed Mar  2 21:00:48 PST 2011

--- openbsd-calendar-cvs/login_cap.h
+++ openbsd-calendar-cvs/login_cap.h
@@ -1,4 +1,4 @@
-/* $NetBSD: login_cap.h,v 1.2 2000/02/04 02:17:16 mjl Exp $ */
+/*	$OpenBSD: login_cap.h,v 1.12 2004/08/09 21:15:08 millert Exp $	*/
 
 /*-
  * Copyright (c) 1995,1997 Berkeley Software Design, Inc. All rights reserved.
@@ -31,16 +31,18 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	BSDI login_cap.h,v 2.10 1997/08/07 21:35:19 prb Exp
+ *	BSDI $From: login_cap.h,v 2.11 1999/09/08 18:11:57 prb Exp $
  */
 
 #ifndef _LOGIN_CAP_H_
 #define _LOGIN_CAP_H_
 
 #define	LOGIN_DEFCLASS		"default"
+#define	LOGIN_DEFSTYLE		"passwd"
 #define	LOGIN_DEFSERVICE	"login"
 #define	LOGIN_DEFUMASK		022
 #define	_PATH_LOGIN_CONF	"/etc/login.conf"
+#define	_PATH_AUTHPROG		"/usr/libexec/auth/login_"
 
 #define	LOGIN_SETGROUP		0x0001	/* Set group */
 #define	LOGIN_SETLOGIN		0x0002	/* Set login */
@@ -49,10 +51,37 @@
 #define	LOGIN_SETRESOURCES	0x0010	/* Set resource limits */
 #define	LOGIN_SETUMASK		0x0020	/* Set umask */
 #define	LOGIN_SETUSER		0x0040	/* Set user */
-#define	LOGIN_SETENV		0x0080	/* Set user environment */
-#define	LOGIN_SETALL 		0x00ff	/* Set all. */
+#define	LOGIN_SETENV		0x0080	/* Set environment */
+#define	LOGIN_SETALL 		0x007f	/* Set all. */
 
-typedef struct {
+#define	BI_AUTH		"authorize"		/* Accepted authentication */
+#define	BI_REJECT	"reject"		/* Rejected authentication */
+#define	BI_CHALLENGE	"reject challenge"	/* Reject with a challenge */
+#define	BI_SILENT	"reject silent"		/* Reject silently */
+#define	BI_REMOVE	"remove"		/* remove file on error */
+#define	BI_ROOTOKAY	"authorize root"	/* root authenticated */
+#define	BI_SECURE	"authorize secure"	/* okay on non-secure line */
+#define	BI_SETENV	"setenv"		/* set environment variable */
+#define	BI_UNSETENV	"unsetenv"		/* unset environment variable */
+#define	BI_VALUE	"value"			/* set local variable */
+#define	BI_EXPIRED	"reject expired"	/* account expired */
+#define	BI_PWEXPIRED	"reject pwexpired"	/* password expired */
+#define	BI_FDPASS	"fd"			/* child is passing an fd */
+
+/*
+ * bits which can be returned by authenticate()/auth_scan()
+ */
+#define	AUTH_OKAY	0x01			/* user authenticated */
+#define	AUTH_ROOTOKAY	0x02			/* authenticated as root */
+#define	AUTH_SECURE	0x04			/* secure login */
+#define	AUTH_SILENT	0x08			/* silent rejection */
+#define	AUTH_CHALLENGE	0x10			/* a challenge was given */
+#define	AUTH_EXPIRED	0x20			/* account expired */
+#define	AUTH_PWEXPIRED	0x40			/* password expired */
+
+#define	AUTH_ALLOW	(AUTH_OKAY | AUTH_ROOTOKAY | AUTH_SECURE)
+
+typedef struct login_cap {
 	char	*lc_class;
 	char	*lc_cap;
 	char	*lc_style;
@@ -62,20 +91,19 @@ typedef struct {
 __BEGIN_DECLS
 struct passwd;
 
-login_cap_t *login_getclass __P((char *));
-login_cap_t *login_getpwclass __P((const struct passwd *));
-void	 login_close __P((login_cap_t *));
-int	 login_getcapbool __P((login_cap_t *, char *, u_int));
-quad_t	 login_getcapnum __P((login_cap_t *, char *, quad_t, quad_t));
-quad_t	 login_getcapsize __P((login_cap_t *, char *, quad_t, quad_t));
-char	*login_getcapstr __P((login_cap_t *, char *, char *, char *));
-quad_t	 login_getcaptime __P((login_cap_t *, char *, quad_t, quad_t));
+login_cap_t *login_getclass(char *);
+void	 login_close(login_cap_t *);
+int	 login_getcapbool(login_cap_t *, char *, unsigned int);
+quad_t	 login_getcapnum(login_cap_t *, char *, quad_t, quad_t);
+quad_t	 login_getcapsize(login_cap_t *, char *, quad_t, quad_t);
+char	*login_getcapstr(login_cap_t *, char *, char *, char *);
+quad_t	 login_getcaptime(login_cap_t *, char *, quad_t, quad_t);
+char	*login_getstyle(login_cap_t *, char *, char *);
 
-int	secure_path __P((char *));
-int	setclasscontext __P((char *, u_int));
-int	setusercontext __P((login_cap_t *, struct passwd *, uid_t, u_int));
+int	secure_path(char *);
+int	setclasscontext(char *, unsigned int);
+int	setusercontext(login_cap_t *, struct passwd *, uid_t, unsigned int);
 
 __END_DECLS
 
-#endif
- 
+#endif /* _LOGIN_CAP_H_ */
--- openbsd-calendar-cvs/tzfile.h
+++ openbsd-calendar-cvs/tzfile.h
@@ -1,97 +1,86 @@
-#ifndef TZFILE_H
-
-#define TZFILE_H
-
-/*
-** This file is in the public domain, so clarified as of
-** 1996-06-05 by Arthur David Olson.
-*/
+/*	$OpenBSD: tzfile.h,v 1.3 1998/03/30 06:53:47 deraadt Exp $	*/
+/*	$NetBSD: tzfile.h,v 1.3 1994/10/26 00:56:37 cgd Exp $	*/
 
 /*
-** This header is for use ONLY with the time conversion code.
-** There is no guarantee that it will remain unchanged,
-** or that it will remain at all.
-** Do NOT copy it to any system include directory.
-** Thank you!
-*/
+ * Copyright (c) 1988 Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Arthur David Olson of the National Cancer Institute.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)tzfile.h	5.10 (Berkeley) 4/3/91
+ */
+
+#ifndef _TZFILE_H_
+#define	_TZFILE_H_
 
 /*
-** ID
-*/
-
-#ifndef lint
-#ifndef NOID
-static char tzfilehid[] = "@(#)tzfile.h 8.1";
-#endif /* !defined NOID */
-#endif /* !defined lint */
-
-/*
-** Information about time zone files.
-*/
-
-#ifndef TZDIR
-#define TZDIR "/system/usr/share/zoneinfo" /* Time zone object file directory */
-#endif /* !defined TZDIR */
-
-#ifndef TZDEFAULT
-#define TZDEFAULT   "localtime"
-#endif /* !defined TZDEFAULT */
-
-#ifndef TZDEFRULES
-#define TZDEFRULES  "posixrules"
-#endif /* !defined TZDEFRULES */
+ * Information about time zone files.
+ */
+			/* Time zone object file directory */
+#define TZDIR		"/usr/share/zoneinfo"
+#define TZDEFAULT	"/etc/localtime"
+#define TZDEFRULES	"posixrules"
 
 /*
 ** Each file begins with. . .
 */
 
-#define TZ_MAGIC    "TZif"
-
 struct tzhead {
-    char    tzh_magic[4];       /* TZ_MAGIC */
-    char    tzh_version[1];     /* '\0' or '2' as of 2005 */
-    char    tzh_reserved[15];   /* reserved--must be zero */
-    char    tzh_ttisgmtcnt[4];  /* coded number of trans. time flags */
-    char    tzh_ttisstdcnt[4];  /* coded number of trans. time flags */
-    char    tzh_leapcnt[4];     /* coded number of leap seconds */
-    char    tzh_timecnt[4];     /* coded number of transition times */
-    char    tzh_typecnt[4];     /* coded number of local time types */
-    char    tzh_charcnt[4];     /* coded number of abbr. chars */
+	char	tzh_reserved[24];	/* reserved for future use */
+	char	tzh_ttisstdcnt[4];	/* coded number of trans. time flags */
+	char	tzh_leapcnt[4];		/* coded number of leap seconds */
+	char	tzh_timecnt[4];		/* coded number of transition times */
+	char	tzh_typecnt[4];		/* coded number of local time types */
+	char	tzh_charcnt[4];		/* coded number of abbr. chars */
 };
 
 /*
 ** . . .followed by. . .
 **
-**  tzh_timecnt (char [4])s     coded transition times a la time(2)
-**  tzh_timecnt (unsigned char)s    types of local time starting at above
-**  tzh_typecnt repetitions of
-**      one (char [4])      coded UTC offset in seconds
-**      one (unsigned char) used to set tm_isdst
-**      one (unsigned char) that's an abbreviation list index
-**  tzh_charcnt (char)s     '\0'-terminated zone abbreviations
-**  tzh_leapcnt repetitions of
-**      one (char [4])      coded leap second transition times
-**      one (char [4])      total correction after above
-**  tzh_ttisstdcnt (char)s      indexed by type; if TRUE, transition
-**                  time is standard time, if FALSE,
-**                  transition time is wall clock time
-**                  if absent, transition times are
-**                  assumed to be wall clock time
-**  tzh_ttisgmtcnt (char)s      indexed by type; if TRUE, transition
-**                  time is UTC, if FALSE,
-**                  transition time is local time
-**                  if absent, transition times are
-**                  assumed to be local time
-*/
-
-/*
-** If tzh_version is '2' or greater, the above is followed by a second instance
-** of tzhead and a second instance of the data in which each coded transition
-** time uses 8 rather than 4 chars,
-** then a POSIX-TZ-environment-variable-style string for use in handling
-** instants after the last transition time stored in the file
-** (with nothing between the newlines if there is no POSIX representation for
-** such instants).
+**	tzh_timecnt (char [4])s		coded transition times a la time(2)
+**	tzh_timecnt (unsigned char)s	types of local time starting at above
+**	tzh_typecnt repetitions of
+**		one (char [4])		coded GMT offset in seconds
+**		one (unsigned char)	used to set tm_isdst
+**		one (unsigned char)	that's an abbreviation list index
+**	tzh_charcnt (char)s		'\0'-terminated zone abbreviations
+**	tzh_leapcnt repetitions of
+**		one (char [4])		coded leap second transition times
+**		one (char [4])		total correction after above
+**	tzh_ttisstdcnt (char)s		indexed by type; if TRUE, transition
+**					time is standard time, if FALSE,
+**					transition time is wall clock time
+**					if absent, transition times are
+**					assumed to be wall clock time
 */
 
 /*
@@ -99,82 +88,67 @@ struct tzhead {
 ** exceed any of the limits below.
 */
 
-#ifndef TZ_MAX_TIMES
-#define TZ_MAX_TIMES    1200
-#endif /* !defined TZ_MAX_TIMES */
+/*
+** The TZ_MAX_TIMES value below is enough to handle a bit more than a
+** year's worth of solar time (corrected daily to the nearest second) or
+** 138 years of Pacific Presidential Election time
+** (where there are three time zone transitions every fourth year).
+*/
+#define TZ_MAX_TIMES	370
 
-#ifndef TZ_MAX_TYPES
+#define NOSOLAR			/* 4BSD doesn't currently handle solar time */
+
 #ifndef NOSOLAR
-#define TZ_MAX_TYPES    256 /* Limited by what (unsigned char)'s can hold */
-#endif /* !defined NOSOLAR */
-#ifdef NOSOLAR
-/*
-** Must be at least 14 for Europe/Riga as of Jan 12 1995,
-** as noted by Earl Chew.
-*/
-#define TZ_MAX_TYPES    20  /* Maximum number of local time types */
-#endif /* !defined NOSOLAR */
-#endif /* !defined TZ_MAX_TYPES */
+#define TZ_MAX_TYPES	256	/* Limited by what (unsigned char)'s can hold */
+#else
+#define TZ_MAX_TYPES	10	/* Maximum number of local time types */
+#endif
 
-#ifndef TZ_MAX_CHARS
-#define TZ_MAX_CHARS    50  /* Maximum number of abbreviation characters */
-                /* (limited by what unsigned chars can hold) */
-#endif /* !defined TZ_MAX_CHARS */
+#define TZ_MAX_CHARS	50	/* Maximum number of abbreviation characters */
 
-#ifndef TZ_MAX_LEAPS
-#define TZ_MAX_LEAPS    50  /* Maximum number of leap second corrections */
-#endif /* !defined TZ_MAX_LEAPS */
+#define	TZ_MAX_LEAPS	50	/* Maximum number of leap second corrections */
 
-#define SECSPERMIN  60
-#define MINSPERHOUR 60
-#define HOURSPERDAY 24
-#define DAYSPERWEEK 7
-#define DAYSPERNYEAR    365
-#define DAYSPERLYEAR    366
-#define SECSPERHOUR (SECSPERMIN * MINSPERHOUR)
-#define SECSPERDAY  ((long) SECSPERHOUR * HOURSPERDAY)
-#define MONSPERYEAR 12
+#define SECSPERMIN	60
+#define MINSPERHOUR	60
+#define HOURSPERDAY	24
+#define DAYSPERWEEK	7
+#define DAYSPERNYEAR	365
+#define DAYSPERLYEAR	366
+#define SECSPERHOUR	(SECSPERMIN * MINSPERHOUR)
+#define SECSPERDAY	((long) SECSPERHOUR * HOURSPERDAY)
+#define MONSPERYEAR	12
 
-#define TM_SUNDAY   0
-#define TM_MONDAY   1
-#define TM_TUESDAY  2
-#define TM_WEDNESDAY    3
-#define TM_THURSDAY 4
-#define TM_FRIDAY   5
-#define TM_SATURDAY 6
+#define TM_SUNDAY	0
+#define TM_MONDAY	1
+#define TM_TUESDAY	2
+#define TM_WEDNESDAY	3
+#define TM_THURSDAY	4
+#define TM_FRIDAY	5
+#define TM_SATURDAY	6
 
-#define TM_JANUARY  0
-#define TM_FEBRUARY 1
-#define TM_MARCH    2
-#define TM_APRIL    3
-#define TM_MAY      4
-#define TM_JUNE     5
-#define TM_JULY     6
-#define TM_AUGUST   7
-#define TM_SEPTEMBER    8
-#define TM_OCTOBER  9
-#define TM_NOVEMBER 10
-#define TM_DECEMBER 11
+#define TM_JANUARY	0
+#define TM_FEBRUARY	1
+#define TM_MARCH	2
+#define TM_APRIL	3
+#define TM_MAY		4
+#define TM_JUNE		5
+#define TM_JULY		6
+#define TM_AUGUST	7
+#define TM_SEPTEMBER	8
+#define TM_OCTOBER	9
+#define TM_NOVEMBER	10
+#define TM_DECEMBER	11
 
-#define TM_YEAR_BASE    1900
+#define TM_YEAR_BASE	1900
 
-#define EPOCH_YEAR  1970
-#define EPOCH_WDAY  TM_THURSDAY
-
-#define isleap(y) (((y) % 4) == 0 && (((y) % 100) != 0 || ((y) % 400) == 0))
+#define EPOCH_YEAR	1970
+#define EPOCH_WDAY	TM_THURSDAY
 
 /*
-** Since everything in isleap is modulo 400 (or a factor of 400), we know that
-**  isleap(y) == isleap(y % 400)
-** and so
-**  isleap(a + b) == isleap((a + b) % 400)
-** or
-**  isleap(a + b) == isleap(a % 400 + b % 400)
-** This is true even if % means modulo rather than Fortran remainder
-** (which is allowed by C89 but not C99).
-** We use this to avoid addition overflow problems.
+** Accurate only for the past couple of centuries;
+** that will probably do.
 */
 
-#define isleap_sum(a, b)    isleap((a) % 400 + (b) % 400)
+#define isleap(y) ((((y) % 4) == 0 && ((y) % 100) != 0) || ((y) % 400) == 0)
 
-#endif /* !defined TZFILE_H */
+#endif /* !_TZFILE_H_ */
