a5c08a9296e110285d593b52880a203bac6aba34
Thomas Dziedzic <gostrc@gmail.com>
2011-03-06 03:00:49 -0800

    updated on Sun Mar  6 03:00:49 PST 2011

--- parboiled/setClasspath.sh
+++ parboiled/setClasspath.sh
@@ -4,4 +4,4 @@
 # This script will set parboiled's classpath variable.
 ##
 
-PARBOILED_CLASSPATH=/usr/share/java/parboiled/parboiled4j.jar
+PARBOILED_CLASSPATH=/usr/share/java/parboiled/parboiled-core.jar:/usr/share/java/parboiled/parboiled-java.jar
