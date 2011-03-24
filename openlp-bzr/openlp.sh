bd6e57c0720ca0873fc2fa7149e99f7841d10613
Thomas Dziedzic <gostrc@gmail.com>
2011-03-24 13:30:50 -0700

    updated on Thu Mar 24 13:30:50 PDT 2011

--- openlp-bzr/openlp.sh
+++ openlp-bzr/openlp.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 for i in libreoffice openoffice.org{,-devel}; do
- if [ -e "/usr/lib/$i/basis3.3/program/uno.py" ]; then
+ if [ -e "/usr/lib/$i/basis-link/program/uno.py" ]; then
   export PYTHONPATH="$PYTHONPATH:/usr/lib/$i/basis-link/program/"
   break
  fi
