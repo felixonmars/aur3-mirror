bd0c8f82ec602fbac647b8082285f1415a9b262a
Thomas Dziedzic <gostrc@gmail.com>
2011-03-30 06:30:49 -0700

    updated on Wed Mar 30 06:30:49 PDT 2011

--- kippo/start.sh
+++ kippo/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-echo -n "Starting kippo in background..."
-su -c "twistd -y kippo.tac -l log/kippo.log --pidfile kippo.pid" kippo
+echo "Starting kippo in background..."
+sudo -u kippo twistd -y kippo.tac -l log/kippo.log --pidfile kippo.pid
