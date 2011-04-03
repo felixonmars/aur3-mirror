60f453e4bf3dbd33ef35dd9ed968ab80ccc72c58
Thomas Dziedzic <gostrc@gmail.com>
2011-04-03 13:30:50 -0700

    updated on Sun Apr  3 13:30:50 PDT 2011

--- bitdefender/bitdefender.sh
+++ bitdefender/bitdefender.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-export PATH=$PATH:/opt/BitDefender-scanner/bin
 export LDPATH="/opt/BitDefender-scanner/var/lib"
 export MANPATH="/opt/BitDefender-scanner/share/man"
-export LDCONFIG_NOTRIGGER=y
