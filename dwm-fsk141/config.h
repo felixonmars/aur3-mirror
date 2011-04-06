ca059c90bb9e61783cd90a71fe4abf50776011dc
Thomas Dziedzic <gostrc@gmail.com>
2011-04-06 15:30:49 -0700

    updated on Wed Apr  6 15:30:49 PDT 2011

--- dwm-fsk141/config.h
+++ dwm-fsk141/config.h
@@ -14,12 +14,12 @@ static const Bool showbar           = True;     /* False means no bar */
 static const Bool topbar            = True;     /* False means bottom bar */
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "web", "8", "9" };
 
 static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       False,       -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 6,       False,       -1 },
 };
 
 /* layout(s) */
