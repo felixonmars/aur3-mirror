b981029ccdb0ab96218f782f7757730a704086e5
Thomas Dziedzic <gostrc@gmail.com>
2011-04-07 08:30:50 -0700

    updated on Thu Apr  7 08:30:50 PDT 2011

--- openarch_openbox_scripts/bindkeys.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-[[ HOME = "" ]] && HOME="/home/liveuser"
-FILE=${HOME}/.keybindings
-zenity --title="Key bindings" --timeout=10 --height=500 --width=280 --text-info --filename="${FILE}"
---
+++ texdro/texdro
@@ -0,0 +1,5 @@
+#!/bin/sh
+java -d32 -classpath com.crackedsun.texdrodesktop.TexdroDesktop -jar "/usr/share/java/texdro/TexdroDesktop.jar" "$@"
+
+
+
