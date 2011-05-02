6470ee202179372a0070535063980c2d3045d518
Thomas Dziedzic <gostrc@gmail.com>
2011-05-02 13:31:11 -0700

    updated on Mon May  2 13:31:11 PDT 2011

--- pcsx2-svn-bin/pcsx2.sh
+++ pcsx2-svn-bin/pcsx2.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-# Launch PCSX2
-cd /opt/pcsx2
-./pcsx2 $*
+# Launch PCSX2 emulator
+if [ -x /opt/pcsx2/pcsx2 ]; then
+	cd /opt/pcsx2
+	./pcsx2 $*
+else 
+	echo "PCSX2 executable not found" && exit 1
+fi
+
