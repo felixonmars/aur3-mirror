ecd56922f19006e62832e2c34ff1bfb09aa1a60a
Thomas Dziedzic <gostrc@gmail.com>
2011-05-04 13:00:53 -0700

    updated on Wed May  4 13:00:53 PDT 2011

--- dofus/UpLauncher.arch.sh
+++ dofus/UpLauncher.arch.sh
@@ -7,12 +7,8 @@ SCRIPT_PATH=`readlink -f $0`
 SCRIPT_DIR=`dirname $SCRIPT_PATH`
 
 if [ "`uname -m`" == "x86_64" ]; then
-	export GTK_PATH=/opt/lib32/usr/lib/gtk-2.0
-	export PANGO_RC_FILE=/opt/lib32/config/pango/pangorc
-	export GDK_PIXBUF_MODULE_FILE=/opt/lib32/config/gdk/gdk-pixbuf.loaders
-	export GTK_IM_MODULE_FILE=/opt/lib32/config/gdk/gdk.immodules
+	export GTK_PATH=/usr/lib32/gtk-2.0
 	export G_FILENAME_ENCODING=UTF-8
 fi
 
 $SCRIPT_DIR/UpLauncher $*
-
