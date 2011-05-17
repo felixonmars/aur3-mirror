ce91873715e468557e6b2684a46de3251e5749c8
Thomas Dziedzic <gostrc@gmail.com>
2011-05-17 04:00:54 -0700

    updated on Tue May 17 04:00:54 PDT 2011

--- pcsx2/pcsx2.sh
+++ pcsx2/pcsx2.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-# Launch PCSX2 in any build mode
-cd /opt/pcsx2/
-for i in pcsx2-dbg pcsx2-dev pcsx2 fail; do
-	if [ -x $i ]; then
-		./$i $*
-		exit $?
-	fi
-done
-# If did not find, then...
-echo "PCSX2 executable not found" && echo 1
-
+# Launch PCSX2
+cd /opt/pcsx2
+./pcsx2 $*
