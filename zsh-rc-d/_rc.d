c800817a48656a146436438dad9095b811fa96c7
Thomas Dziedzic <gostrc@gmail.com>
2011-05-18 03:30:54 -0700

    updated on Wed May 18 03:30:54 PDT 2011

--- zsh-rc-d/_rc.d
+++ zsh-rc-d/_rc.d
@@ -1,6 +1,36 @@
 #compdef rc.d
 
-_arguments \
-    "1: :(start stop restart list help)" \
-    "*: :_services"
+_rc.d () {
+    local curcontext="$curcontext" state line
+    typeset -A opt_args
 
+    _arguments \
+        "1: :->action" \
+        "*: :->service"
+
+    case $state in
+        action)
+            _arguments "1:action:(list help start stop restart)"
+            ;;
+        service)
+            local action="$words[2]"
+            curcontext="${curcontext%:*:*}:rc.d-${action}:"
+
+            case $action in
+                list)
+                    _arguments "*: :"
+                    ;;
+                help)
+                    _arguments "*: :"
+                    ;;
+                *)
+                    _arguments "*: :_services"
+                    ;;
+            esac
+            ;;
+    esac
+}
+
+_rc.d "$@"
+
+# vim: filetype=zsh
