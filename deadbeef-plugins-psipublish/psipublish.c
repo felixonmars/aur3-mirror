159a9452d22b31dacea9bea3c6247c323988b2cb
Thomas Dziedzic <gostrc@gmail.com>
2011-05-21 00:30:56 -0700

    updated on Sat May 21 00:30:56 PDT 2011

--- deadbeef-plugins-psipublish/psipublish.c
+++ deadbeef-plugins-psipublish/psipublish.c
@@ -13,12 +13,12 @@ void updatetune(const char *title, const char *artist, const char *album, int le
     FILE *tunefile;
     tunefile = fopen(tunefilepath, "w");
     if (tunefile) {
-        fprintf(tunefile, "%s%s%s\n%s\n%s\n%s\n%d\n", title, deadbeef->conf_get_int("psipublish.showplayer", 0) ? " [DeadBeef]" : "", isplayed ? "" : " [PAUSED]", artist, album, title, length);
+        fprintf(tunefile, "%s%s%s\n%s\n%s\n%s\n%d\n", title, deadbeef->conf_get_int("psipublish.showplayer", 0) ? " [DeaDBeeF]" : "", isplayed ? "" : " [PAUSED]", artist, album, title, length);
         fclose(tunefile); 
     }
 }
 
-static int psipublish_songstarted(DB_event_track_t *ev, uintptr_t data) {
+static int psipublish_songstarted(ddb_event_track_t *ev) {
     if (deadbeef->conf_get_int("psipublish.enable", 0)) {
         DB_playItem_t *track = ev->track;
         if (track) {
@@ -29,17 +29,17 @@ static int psipublish_songstarted(DB_event_track_t *ev, uintptr_t data) {
     return 0;
 }
 
-static int psipublish_songfinished(DB_event_track_t *ev, uintptr_t data) {
+static int psipublish_songfinished(ddb_event_track_t *ev) {
     remove(tunefilepath);
     return 0;
 }
 
 
-static int psipublish_paused(DB_event_state_t *ev, uintptr_t data) {
+static int psipublish_paused() {
     if (deadbeef->conf_get_int("psipublish.enable", 0)) {
         DB_playItem_t *track = deadbeef->streamer_get_playing_track();
         if (track) {
-            isplayed = !(ev->state);
+            isplayed = deadbeef->get_output()->state() == OUTPUT_STATE_PLAYING;
             updatetune(deadbeef->pl_find_meta(track, "title"), deadbeef->pl_find_meta(track, "artist"), deadbeef->pl_find_meta(track, "album"), deadbeef->pl_get_item_duration(track));
             deadbeef->pl_item_unref(track);
         }
@@ -47,19 +47,27 @@ static int psipublish_paused(DB_event_state_t *ev, uintptr_t data) {
     return 0; 
 }
 
+static int psipublish_pluginmessage(uint32_t id, uintptr_t ctx, uint32_t p1, uint32_t p2) {
+    switch (id) {
+    case DB_EV_SONGSTARTED:
+        psipublish_songstarted((ddb_event_track_t *)ctx);
+        break;
+    case DB_EV_SONGFINISHED:
+        psipublish_songfinished((ddb_event_track_t *)ctx);
+        break;
+    case DB_EV_PAUSED:
+        psipublish_paused();
+        break;
+    }
+    return 0;
+}
 
 int psipublish_pluginstart(void) {
     sprintf(tunefilepath, "%s/.psi/tune", getenv("HOME"));
-    deadbeef->ev_subscribe(DB_PLUGIN(&plugin), DB_EV_SONGSTARTED, DB_CALLBACK(psipublish_songstarted), 0);
-    deadbeef->ev_subscribe(DB_PLUGIN(&plugin), DB_EV_SONGFINISHED, DB_CALLBACK(psipublish_songfinished), 0);
-    deadbeef->ev_subscribe(DB_PLUGIN(&plugin), DB_EV_PAUSED, DB_CALLBACK(psipublish_paused), 0);
     return 0;
 }
 
 int psipublish_pluginstop (void) {
-    deadbeef->ev_unsubscribe(DB_PLUGIN(&plugin), DB_EV_SONGSTARTED, DB_CALLBACK(psipublish_songstarted), 0);
-    deadbeef->ev_unsubscribe(DB_PLUGIN(&plugin), DB_EV_SONGFINISHED, DB_CALLBACK(psipublish_songfinished), 0);
-    deadbeef->ev_unsubscribe(DB_PLUGIN(&plugin), DB_EV_PAUSED, DB_CALLBACK(psipublish_paused), 0);
     remove(tunefilepath);
     return 0;
 }
@@ -73,15 +81,14 @@ DB_misc_t plugin = {
     DB_PLUGIN_SET_API_VERSION
     .plugin.type = DB_PLUGIN_MISC,
     .plugin.version_major = 1,
-    .plugin.version_minor = 1,
+    .plugin.version_minor = 2,
     .plugin.id = "psipublish",
     .plugin.name = "Psi IM Publish Tune",
     .plugin.descr = "Now playing for Psi IM",
-    .plugin.author = "Joker-jar",
-    .plugin.email = "joker-jar@yandex.ru",
     .plugin.website = "",
     .plugin.start = psipublish_pluginstart,
     .plugin.stop = psipublish_pluginstop,
+    .plugin.message = psipublish_pluginmessage,
     .plugin.configdialog = settings_dlg
 };
 
