ff5605e05303409f8a4a54d293e43dc9f4f28ef7
Thomas Dziedzic <gostrc@gmail.com>
2011-06-03 14:10:19 +0000

    updated on Fri Jun  3 14:10:18 UTC 2011

--- xsbt/xsbt.sh
+++ xsbt/xsbt.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 if [ -z "$JAVACMD" ] ; then
-  if [ -n "$JAVA_HOME"  ] ; then
-    if [ -x "$JAVA_HOME/jre/sh/java" ] ; then
-      # IBM's JDK on AIX uses strange locations for the executables
-      JAVACMD="$JAVA_HOME/jre/sh/java"
-    else
-      JAVACMD="$JAVA_HOME/bin/java"
-    fi
-  else
-    JAVACMD="`which java`"
-  fi
+		if [ -n "$JAVA_HOME"  ] ; then
+				if [ -x "$JAVA_HOME/jre/sh/java" ] ; then
+						# IBM's JDK on AIX uses strange locations for the executables
+						JAVACMD="$JAVA_HOME/jre/sh/java"
+				else
+						JAVACMD="$JAVA_HOME/bin/java"
+				fi
+		else
+				JAVACMD="`which java`"
+		fi
 fi
 if [ -z "$JAVA_ARGS" ]
 then
