e5c292040740a8f585f6009331e61a9fdfba64d4
Thomas Dziedzic <gostrc@gmail.com>
2011-07-16 06:30:59 +0000

    updated on Sat Jul 16 06:30:59 UTC 2011

--- libmatthew-java/setClasspath.sh
+++ libmatthew-java/setClasspath.sh
@@ -5,6 +5,6 @@
 ##
 
 LIBMATTHEW_HOME=/usr/share/java/libmatthew
-LIBMATTHEW_CLASSPATH=${LIBMATTHEW_HOME}/unix.jar:${LIBMATTHEW_HOME}/debug-disable.jar:${LIBMATTHEW_HOME}/hexdump.jar:${LIBMATTHEW_HOME}/dbus.jar:${LIBMATTHEW_HOME}/dbus-bin.jar
-LIBMATTHEW_DEBUG_CLASSPATH=${LIBMATTHEW_CLASSPATH}/unix.jar:${LIBMATTHEW_PATH}/debug-enable.jar:${LIBMATTHEW_PATH}/hexdump.jar:${LIBMATTHEW_PATH}/dbus.jar:${LIBMATTHEW_PATH}/dbus-bin.jar
+UNIX_SOCKETS=${LIBMATTHEW_HOME}/unix.jar:${LIBMATTHEW_HOME}/debug-disable.jar:${LIBMATTHEW_HOME}/hexdump.jar
+UNIX_SOCKET_DEBUG=${LIBMATTHEW_HOME}/unix.jar:${LIBMATTHEW_HOME}/debug-enable.jar:${LIBMATTHEW_HOME}/hexdump.jar
 
