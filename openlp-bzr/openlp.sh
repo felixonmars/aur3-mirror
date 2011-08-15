04b583806f0b22ba69df02b8e92a5069e87f49fc
Thomas Dziedzic <gostrc@gmail.com>
2011-08-15 00:01:26 +0000

    updated on Mon Aug 15 00:01:25 UTC 2011

--- openlp-bzr/openlp.sh
+++ openlp-bzr/openlp.sh
@@ -1,2 +1,4 @@
-[ -e "/usr/lib/libreoffice/basis-link/program/uno.py" ] && \
+if [ -e "/usr/lib/libreoffice/basis-link/program/uno.py" ]; then
  export PYTHONPATH="$PYTHONPATH:/usr/lib/libreoffice/basis-link/program/"
+ export URE_BOOTSTRAP="file:///usr/lib/libreoffice/program/fundamentalrc"
+fi
