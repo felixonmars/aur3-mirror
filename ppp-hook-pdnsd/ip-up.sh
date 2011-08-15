703550d836f78b3ee8c488df667e7b0b97384ef1
Thomas Dziedzic <gostrc@gmail.com>
2011-08-15 09:30:59 +0000

    updated on Mon Aug 15 09:30:59 UTC 2011

--- ppp-hook-pdnsd/ip-up.sh
+++ ppp-hook-pdnsd/ip-up.sh
@@ -4,5 +4,5 @@ if [ -x "/usr/sbin/pdnsd-ctl" ]; then
 	if [ -n "${DNS2}" ]; then
 		DNS1="${DNS1},${DNS2}"
 	fi
-	/usr/sbin/pdnsd-ctl server 0 up ${DNS1}
+	/usr/sbin/pdnsd-ctl server dynamic up ${DNS1}
 fi
