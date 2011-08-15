2df3868570c9c8c0accc1fdea79b97a11dbd75f0
Thomas Dziedzic <gostrc@gmail.com>
2011-08-15 07:31:01 +0000

    updated on Mon Aug 15 07:31:01 UTC 2011

--- utserver/utserver.rc.d
+++ utserver/utserver.rc.d
@@ -2,38 +2,41 @@
 
 . /etc/rc.conf
 . /etc/rc.d/functions
+
 . /etc/conf.d/utserver
 
-[[ -f /var/run/utserver.pid ]] && PID=$(cat /var/run/utserver.pid)
-[[ -d /proc/$PID ]] || PID=
+PIDFILE=/var/run/utserver/utserver.pid
+
+[ -r "$PIDFILE" ] && PID=$(cat "$PIDFILE")
+[ -n "$PID" ] && PID=$(pidof -s /usr/share/utserver/utserver)
 
 case "$1" in
     start)
-        stat_busy "Starting uTorrent-server Daemon"
-        [[ -z $PID ]] && su -l -s /bin/sh -c "/usr/bin/utserver >/dev/null" $UTSERVER_USER
+        stat_busy "Starting uTorrent-server"
+        su -l -s /bin/sh -c "/usr/bin/utserver >/dev/null" $UTSERVER_USER
         if [ $? -gt 0 ]; then
             stat_fail
         else
-            sleep 1
-            echo $(pgrep -u $UTSERVER_USER utserver) > /var/run/utserver.pid
             add_daemon utserver
             stat_done
         fi
         ;;
     stop)
-        stat_busy "Stopping uTorrent-server Daemon"
-        [[ ! -z $PID ]] && kill $PID &> /dev/null
+        stat_busy "Stopping uTorrent-server"
+        [ -n "$PID" ] && kill $PID &> /dev/null
         if [ $? -gt 0 ]; then
             stat_fail
         else
-            rm /var/run/utserver.pid
+            while [ -d /proc/"$PID" ]; do
+                sleep 1
+            done
+            rm -f "$PIDFILE"
             rm_daemon utserver
             stat_done
         fi
         ;;
     restart)
         $0 stop
-        sleep 1
         $0 start
         ;;
     *)
