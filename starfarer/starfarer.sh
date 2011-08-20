ada6c2eab69b4db88155d9b0559579ac89b48f9c
Thomas Dziedzic <gostrc@gmail.com>
2011-08-20 14:01:11 +0000

    updated on Sat Aug 20 14:01:11 UTC 2011

--- starfarer/starfarer.sh
+++ starfarer/starfarer.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
-if [ ! -d ~/.starfarer ]
-  then mkdir ~/.starfarer
-fi
-
-if [ ! -d ~/.starfarer/saves ]
-  then mkdir ~/.starfarer/saves
-fi
-
-if [ ! -d ~/.starfarer/screenshots ]
-  then mkdir ~/.starfarer/screenshots
-fi
-
-if [ ! -d ~/.starfarer/mods ]
-  then mkdir ~/.starfarer/mods
-fi
+[ ! -d ~/.starfarer ] && mkdir ~/.starfarer
+[ ! -d ~/.starfarer/saves ] && mkdir ~/.starfarer/saves
+[ ! -d ~/.starfarer/screenshots ] && mkdir ~/.starfarer/screenshots
+[ ! -d ~/.starfarer/mods ] && mkdir ~/.starfarer/mods
 
 for name in /usr/share/java/starfarer/*.jar ; do
   CP=$CP:$name
@@ -24,5 +13,6 @@ cd /usr/share/starfarer
 SAVEPATH=~/.starfarer/saves
 MODPATH=~/.starfarer/mods
 SCRPATH=~/.starfarer/screenshots
+LOGPATH=~/.starfarer
 
-exec java -Djava.library.path=/usr/lib/starfarer -Xms256m -Xmx256m -cp $CP -Dcom.fs.starfarer.settings.paths.saves=$SAVEPATH -Dcom.fs.starfarer.settings.paths.screenshots=$SCRPATH -Dcom.fs.starfarer.settings.paths.mods=$MODPATH com.fs.starfarer.StarfarerLauncher
+exec java -Djava.library.path=/usr/lib/starfarer -Xms256m -Xmx256m -cp $CP -Dcom.fs.starfarer.settings.paths.saves=$SAVEPATH -Dcom.fs.starfarer.settings.paths.screenshots=$SCRPATH -Dcom.fs.starfarer.settings.paths.mods=$MODPATH -Dcom.fs.starfarer.settings.paths.logs=$LOGPATH com.fs.starfarer.StarfarerLauncher
