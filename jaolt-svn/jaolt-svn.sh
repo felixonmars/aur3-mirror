961204bda304f1f48f2ebc8fa2b02b6d1b67ae12
Thomas Dziedzic <gostrc@gmail.com>
2011-08-28 19:30:55 +0000

    updated on Sun Aug 28 19:30:54 UTC 2011

--- jaolt-svn/jaolt-svn.sh
+++ jaolt-svn/jaolt-svn.sh
@@ -1,2 +1,7 @@
-#!/bin/sh
-java -Xmx512M $* -jar /usr/share/java/jaolt/launcher.jar
+#!/bin/bash
+
+DIR="/usr/share/java"
+CLASSPATH=$DIR/jgoodies-looks/looks-*.jar:$DIR/jgoodies-forms/forms-*.jar:$DIR/jdom/jdom.jar:$DIR/hsqldb.jar:$DIR/jaolt/launcher.jar
+
+java -classpath $CLASSPATH -Xmx512M $* de.shandschuh.jaolt.launcher.ApplicationLauncher
+
