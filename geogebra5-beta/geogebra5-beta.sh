0950868711294478a26f0a8b6c0213c1e28bc665
Thomas Dziedzic <gostrc@gmail.com>
2011-08-28 10:00:54 +0000

    updated on Sun Aug 28 10:00:54 UTC 2011

--- geogebra5-beta/geogebra5-beta.sh
+++ geogebra5-beta/geogebra5-beta.sh
@@ -1,65 +1,82 @@
-#!/bin/sh
-# simple script to start GeoGebra
+#!/bin/bash
+#---------------------------------------------
+# Script to start GeoGebra
+#---------------------------------------------
+
+#---------------------------------------------
+# Define usage function
 
 func_usage()
 {
 cat << _USAGE
-Usage: geogebra [Java-options] [GeoGebra-options] [FILE]
+Usage: $GG_SCRIPTNAME [Java-options] [GeoGebra-options] [FILE]
 
 GeoGebra - Dynamic mathematics software
 
 Java options:
-  -Xms<size>                       set initial Java heap size
-  -Xmx<size>                       set maximum Java heap size
+  -Xms<size>                        Set initial Java heap size
+  -Xmx<size>                        Set maximum Java heap size
 
 GeoGebra options:
-  --help                           print this help message
-  --language=<iso_code>            set language using locale code, e.g. en, de_AT
-  --showAlgebraInput=<boolean>     show/hide algebra input field
-  --showAlgebraWindow=<boolean>    show/hide algebra window
-  --showSpreadsheet=<boolean>      show/hide spreadsheet
-  --fontSize=<number>              set default font size
-  --enableUndo=<boolean>           enable/disable Undo
+  --help                            Show this help message
+  --v                               Show version information
+  --language=<iso_code>             Set language using locale code, e.g. en, de_AT
+  --showAlgebraInput=<boolean>      Show/hide algebra input field
+  --showAlgebraInputTop=<boolean>   Show algebra input field at top/bottom
+  --showAlgebraWindow=<boolean>     Show/hide algebra window
+  --showSpreadsheet=<boolean>       Show/hide spreadsheet
+  --showCAS=<boolean>               Show/hide CAS window
+  --showSplash=<boolean>            Enable/disable the splash screen
+  --enableUndo=<boolean>            Enable/disable Undo
+  --fontSize=<number>               Set default font size
+  --showAxes=<boolean>              Show/hide coordinate axes
+  --settingsFile=<path>|<filename>  Load/save settings from/in a local file
+  --resetSettings                   Reset current settings
+  --CAS=[MPREDUCE|MAXIMA|MATHPIPER] Select which CAS to use, default MPREDUCE
+  --maximaPath=<path>               Specify where Maxima is installed and select Maxima as current CAS
+  --antiAliasing=<boolean>          Turn anti-aliasing on/off
+  --regressionFile=<filename>       Export textual representations of dependent objects, then exit
 _USAGE
 }
 
-# check for option --help and pass memory options to Java, others to GeoGebra
+#---------------------------------------------
+# Check for option --help and pass memory options to Java, others to GeoGebra
+
 for i in "$@"; do
 	case "$i" in
 	--help | --hel | --he | --h )
 		func_usage; exit 0 ;;
 	esac
-	if [ $(expr match "$i" ".*-Xm") -eq 0 ]; then
-		if [ $(expr match "$i" ".*--") -eq 0 ]; then
-			GGB_PATH="$i"
-		else
-			if [ -z "$GG_OPTS" ]; then
-				GG_OPTS="$i"
-			else
-				GG_OPTS="$GG_OPTS $i"
-			fi
-		fi
-	else
+	if [ $(expr match "$i" '.*-Xm') -ne 0 ]; then
 		if [ -z "$JAVA_OPTS" ]; then
 			JAVA_OPTS="$i"
 		else
 			JAVA_OPTS="$JAVA_OPTS $i"
 		fi
+		shift $((1))
+	else
+		if [ $(expr match "$i" '.*--') -ne 0 ]; then
+			if [ -z "$GG_OPTS" ]; then
+				GG_OPTS="$i"
+			else
+				GG_OPTS="$GG_OPTS $i"
+			fi
+			shift $((1))
+		fi
 	fi
 done
 
-# if memory not set, change to GeoGebra defaults
-if [ $(expr match "$JAVA_OPTS" ".*-Xmx") -eq 0 ]; then
+#---------------------------------------------
+# If memory not set, change to GeoGebra defaults
+
+if [ $(expr match "$JAVA_OPTS" '.*-Xmx') -eq 0 ]; then
 	JAVA_OPTS="$JAVA_OPTS -Xmx512m"
 fi
-
-if [ $(expr match "$JAVA_OPTS" ".*-Xms") -eq 0 ]; then
+if [ $(expr match "$JAVA_OPTS" '.*-Xms') -eq 0 ]; then
 	JAVA_OPTS="$JAVA_OPTS -Xms32m"
 fi
 
-# run
-if [ -z "$GGB_PATH" ]; then
-	exec java $JAVA_OPTS -jar /usr/share/java/geogebra5-beta/geogebra.jar --showSplash=false $GG_OPTS
-else
-	exec java $JAVA_OPTS -jar /usr/share/java/geogebra5-beta/geogebra.jar --showSplash=false $GG_OPTS "$GGB_PATH"
-fi
+#---------------------------------------------
+# Run
+
+exec java $JAVA_OPTS -jar "/usr/share/java/geogebra5-beta/geogebra.jar" $GG_OPTS "$@"
