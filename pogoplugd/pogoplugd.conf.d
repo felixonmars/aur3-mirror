587deb60efd904f2aa28a9aff8d9d52c2e337a8f
Thomas Dziedzic <gostrc@gmail.com>
2011-09-01 08:01:00 +0000

    updated on Thu Sep  1 08:01:00 UTC 2011

--- pogoplugd/pogoplugd.conf.d
+++ pogoplugd/pogoplugd.conf.d
@@ -1,5 +1,8 @@
 # The user, under which pogoplugfs will run
-USER='sara'
+USER='yourusername'
+
+# Mountpoint of pogoplug files
+MOUNT='locationofmountpoint'
 
 # Arguments to be passed to the pogoplugd daemon
-POGOPLUGD_ARGS="--mountpoint /media/WDMyPassport"
+POGOPLUGD_ARGS="--mountpoint $MOUNT"
