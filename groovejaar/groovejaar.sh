fa0f048ab52b010cc9cf94e8bee1aedb5b6de05c
Thomas Dziedzic <gostrc@gmail.com>
2011-09-02 02:30:59 +0000

    updated on Fri Sep  2 02:30:59 UTC 2011

--- groovejaar/groovejaar.sh
+++ groovejaar/groovejaar.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-GROOVEJAAR_HOME="/usr/share/groovejaar"
+GROOVEJAAR_HOME="/home/$USER/.groovejaar"
 cd $GROOVEJAAR_HOME
 "$JAVA_HOME/bin/java" -jar '/usr/share/java/groovejaar/GrooveJaar.jar'
