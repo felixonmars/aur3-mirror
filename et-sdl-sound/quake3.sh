00d14038ef9c0d85546a25154618421833122613
Thomas Dziedzic <gostrc@gmail.com>
2011-09-23 23:00:59 +0000

    updated on Fri Sep 23 23:00:59 UTC 2011

--- et-sdl-sound/quake3.sh
+++ et-sdl-sound/quake3.sh
@@ -2,4 +2,4 @@
 cd "/opt/quake3"
 export LD_PRELOAD="et-sdl-sound.so ${LD_PRELOAD}"
 [ -x ./quake3.x86 ] && exec ./quake3.x86 $*
-[ -x ./ioquake3.i386] && exec ./ioquake3.i386 $*
+[ -x ./ioquake3.x86_64] && exec ./ioquake3.x86_64 $*
--- et-sdl-sound/true-combat.sh
+++ et-sdl-sound/true-combat.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd "/opt/enemy-territory"
-LD_PRELOAD="et-sdl-sound.so ${LD_PRELOAD}" exec ./et.x86 +set fs_game tcetest $*
+LD_PRELOAD="et-sdl-sound.so ${LD_PRELOAD}" exec ./et +set fs_game tcetest $*
 
