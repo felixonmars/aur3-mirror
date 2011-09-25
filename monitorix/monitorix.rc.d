c048a64fde2f986026a37ae25e6ff1497761fbfe
Thomas Dziedzic <gostrc@gmail.com>
2011-09-25 19:00:59 +0000

    updated on Sun Sep 25 19:00:59 UTC 2011

--- monitorix/monitorix.rc.d
+++ monitorix/monitorix.rc.d
@@ -3,48 +3,48 @@
 . /etc/rc.conf
 . /etc/rc.d/functions
 
-pidfile=/run/monitorix.pid
+pidfile="/run/monitorix.pid"
 if [[ -r $pidfile ]]; then
 	read -r PID < "$pidfile"
 	if [[ ! -d /proc/$PID ]]; then
-		# stale pidfile
-		unset PID
-		rm -f "$pidfile"
+		unset PID && rm -f "$pidfile" # stale pidfile
 	fi
 fi
 
 args=(-c /etc/monitorix.conf -p "$pidfile")
 
 case "$1" in
-start)
-	stat_busy "Starting Monitorix"
-	if [[ -z $PID ]] && /usr/bin/monitorix "${args[@]}"; then
-		add_daemon monitorix
-		stat_done
-	else
-		stat_fail
-		exit 1
-	fi
-;;
+	start)
+		stat_busy "Starting Monitorix"
+		# start sync-monitorix if installed - https://aur.archlinux.org/packages.php?ID=34134
+		[[ -x /usr/bin/sync-monitorix ]] && /usr/bin/sync-monitorix
+		if [[ -z $PID ]] && /usr/bin/monitorix "${args[@]}"; then
+			add_daemon monitorix
+			stat_done
+		else
+			stat_fail
+			exit 1
+		fi
+		;;
 
-stop)
-	stat_busy "Stopping Monitorix"
-	if [[ $PID ]] && kill $PID &> /dev/null; then
-		rm_daemon monitorix
-		[[ -f $pidfile ]] && rm -f $pidfile
-		stat_done
-	else
-		stat_fail
-		exit 1
-	fi
-;;
+	stop)
+		stat_busy "Stopping Monitorix"
+		if [[ $PID ]] && kill $PID &> /dev/null; then
+			rm_daemon monitorix
+			[[ -f $pidfile ]] && rm -f $pidfile
+			stat_done
+		else
+			stat_fail
+			exit 1
+		fi
+		;;
 
-restart)
-	$0 stop
-	sleep 1
-	$0 start
-	;;
+	restart)
+		$0 stop
+		sleep 1
+		$0 start
+		;;
 
-*)
-	echo "usage: $0 {start|stop|restart}"
+	*)
+		echo "usage: $0 {start|stop|restart}"
 esac
