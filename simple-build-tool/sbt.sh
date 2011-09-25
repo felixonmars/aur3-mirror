12a6bffee35835527d429c1d1bbba9ecafaf1aa3
Thomas Dziedzic <gostrc@gmail.com>
2011-09-25 17:30:56 +0000

    updated on Sun Sep 25 17:30:56 UTC 2011

--- simple-build-tool/sbt.sh
+++ simple-build-tool/sbt.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-"$JAVA_HOME/bin/java" -Xmx512M -jar /usr/share/java/sbt-launch-0.7.7.jar "$@"
+if [[ -z "$SBT_OPTS" ]]; then
+  SBT_OPTS="-Xms512m -Xmx1024m -XX:ReservedCodeCacheSize=64m -XX:MaxPermSize=256m -Xss2m -XX:+CMSClassUnloadingEnabled -Dfile.encoding=UTF-8"
+fi
+"$JAVA_HOME/bin/java" $SBT_OPTS -jar /usr/share/java/sbt-launch.jar "$@"
