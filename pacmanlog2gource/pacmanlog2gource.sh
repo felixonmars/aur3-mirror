b52dc31904431c679afad91120489ca971817ac3
Thomas Dziedzic <gostrc@gmail.com>
2011-10-10 20:00:52 +0000

    updated on Mon Oct 10 20:00:52 UTC 2011

--- pacmanlog2gource/pacmanlog2gource.sh
+++ pacmanlog2gource/pacmanlog2gource.sh
@@ -20,7 +20,15 @@
 
 #set -x
 
-#timer functios
+# variables
+
+WORKINGDIR=~/.pacmanlog2gource
+
+LOGTOBEPROCESSED=${WORKINGDIR}/pacman_purged.log
+
+
+
+# timer functions
 
 timestart()
 {
@@ -33,35 +41,68 @@ timeend()
 	TDG=`calc $TEG - $TSG`
 }
 
-# this is our temporary file
-TMPFILE=/tmp/pacman_clean.log
 
-#now we actually start running the script
+
 timestart
 
+# print the version into a file so we handle file formats being out of date properly later
+
+
+#check if we already have a working dir, of not, create one
+if [ -d ${WORKINGDIR} ] ; then
+	echo "Working directory exists."
+else
+	echo "No working directory found, creating one. \( ${WORKINGDIR} \)"
+	mkdir ${WORKINGDIR}
+fi
+
+echo "0.6" > ${WORKINGDIR}/version
+
+
+# make the file
+# TODO: we should probably not touch it but use if/else to check if its there, see above
+touch ${WORKINGDIR}/pacman_now.log
+
+# copy the pacmam log as pacman_tmp to our working dir.
+# this way, log entries that have been made while the script run won't get lost' so we can proceed it later
+
+cp /var/log/pacman.log ${WORKINGDIR}/pacman_tmp.log
+
+# we only want to proceed new entries, old ones are already included in the log
+diff -u ${WORKINGDIR}/pacman_now.log /var/log/pacman.log | grep "^+" | sed -e 's/^+//' > ${WORKINGDIR}/proceed.log
+
+
+
+
+# this is our temporary file
+
+#now we actually start running the script
+
+
 # get lines and size of the pacman log
-ORIGSIZE=`du /var/log/pacman.log | awk '{print $1}'`
-ORIGLINES=`cat /var/log/pacman.log | wc -l`
+ORIGSIZE=`du ${WORKINGDIR}/proceed.log | awk '{print $1}'`
+ORIGLINES=`cat ${WORKINGDIR}/proceed.log | wc -l`
 
-echo "Purging pacman.log (${ORIGLINES} lines, ${ORIGSIZE}kB) and saving the result to /tmp/."
-cat /var/log/pacman.log | sed -e 's/\[/\n[/g' | sed -e '/^$/d' | grep "]\ installed\|]\ upgraded\|]\ removed"  > ${TMPFILE}
+echo "Purging pacman.log ( ${ORIGLINES} lines, ${ORIGSIZE}kB) and saving the result to /tmp/."
+cat  ${WORKINGDIR}/proceed.log | sed -e 's/\[/\n[/g' | sed -e '/^$/d' | grep "]\ installed\|]\ upgraded\|]\ removed"  > ${LOGTOBEPROCESSED}
 
-PURGEDONESIZE=`du ${TMPFILE} | awk '{print $1}'`
-PURGEDONELINES=`cat ${TMPFILE} | wc -l`
+PURGEDONESIZE=`du ${LOGTOBEPROCESSED} | awk '{print $1}'`
+PURGEDONELINES=`cat ${LOGTOBEPROCESSED} | wc -l`
 
 
 LINE=1
 LINEPRCOUT=1
-MAXLINES=`cat ${TMPFILE} | wc -l`
+MAXLINES=`cat ${LOGTOBEPROCESSED} | wc -l`
 
 echo -e "Processing ${MAXLINES} lines of purged log (${PURGEDONESIZE}kb)...\n"
 
-# proceed each line of TEMPFILE and extract important information
+# proceed each line of LOGTOBEPROCESSED and extract important information
+
 while [ "$LINE" -le "$MAXLINES" ]; do
 #### processing the log ####
 
 # the line we are on
-	CURLINE=`cat ${TMPFILE} | awk NR==${LINE}`
+	CURLINE=`cat ${LOGTOBEPROCESSED} | awk NR==${LINE}`
 # the date of the entry
 	DATE=`echo ${CURLINE} | grep -o "[0-9]\{4\}\-[0-9][0-9]\-[0-9]\{2\}\ [0-9]\{2\}\:[0-9]\{2\}"`
 # convert the date into unix time
@@ -72,7 +113,7 @@ while [ "$LINE" -le "$MAXLINES" ]; do
 	PKG=`echo ${CURLINE} | awk '{print $4}'`
 # add extensions to the package name
 # this way we can have the packages grouped and nicely colored in gource
-	if [[ `echo "${PKG}" | grep -o "lib" | grep -v "libreoffice" | head -n1` == "lib" ]] ; then 
+	if [[ `echo "${PKG}" | grep -o "lib" | grep -v "libreoffice" | head -n1` == "lib" ]] ; then
 		PKG=`echo "lib/${PKG}.lib"`
 	else
 		if [[ `echo "${PKG}" | grep -o "xorg" | head -n1` == "xorg" ]] ; then
@@ -174,7 +215,7 @@ while [ "$LINE" -le "$MAXLINES" ]; do
 # yes, the code above sucks
 
 # write the important stuff into our logfile
-	echo "${UDATE}|root|${STATE}|${PKG}" | grep "^[0-9]*|root|A|\|^[0-9]*|root|D|\|^[0-9]*|root|M|" >> pacman_gource.log
+	echo "${UDATE}|root|${STATE}|${PKG}" | grep "^[0-9]*|root|A|\|^[0-9]*|root|D|\|^[0-9]*|root|M|" >> ${WORKINGDIR}/pacman_gource.log
 
 # here we print how log the script already runs and try to estimate how log it will run until finished
 # but we only update this every 250 lines
@@ -195,6 +236,11 @@ while [ "$LINE" -le "$MAXLINES" ]; do
 	LINEPERCOUT=`expr ${LINEPERCOUT} + 1`
 
 done
+
+
+
+
+
 # time it took to run the script
 TIMEFINAL=`echo "${TDG}" | grep -o "[0-9]*\.\?[0-9]\?[0-9]" | head -n1`
 
@@ -204,9 +250,14 @@ echo "Output file is ./pacman_gource.log"
 
 # this is how we can visualize the log
 echo "If you have \"gource\" installed, run"
-echo -e "\tgource ./pacman_gource.log -1200x720 -key --camera-mode overview --highlight-all-users -i 0 -a 0.001 -s 0.5 --hide progress,mouse --stop-at-end --max-files 99999999999 --max-file-lag 0.00001"
+echo -e "\tgource ${WORKINGDIR}/pacman_gource${VERSION}.log -1200x720 -key --camera-mode overview --highlight-all-users -i 0 -a 0.001 -s 0.5 --hide progress,mouse --stop-at-end --max-files 99999999999 --max-file-lag 0.00001"
 echo -e "to visualize the log using gource.\n"
 echo "If you additionally want to make a video of the visualization and have the needed programs installed, append"
 echo -e "\t--output-ppm-stream - | ffmpeg  -f image2pipe -vcodec ppm -i - -vcodec libtheora -y -b 100000K -r 30  -threads 2 pacmanlog2gource_`date +%b\_%d\_%Y`.ogv"
 echo "to the first command."
 echo "\"gource -H\" will give you more information on how to use gource."
+
+
+mv ${WORKINGDIR}/pacman_tmp.log  ${WORKINGDIR}/pacman_now.log
+
+rm ${WORKINGDIR}/pacman_purged.log ${WORKINGDIR}/proceed.log
