51a696809153a757842d28fa3a428259e9225822
Thomas Dziedzic <gostrc@gmail.com>
2011-10-12 10:30:59 +0000

    updated on Wed Oct 12 10:30:58 UTC 2011

--- gnome-shell-extension-icontopbar/extension.js
+++ gnome-shell-extension-icontopbar/extension.js
@@ -11,16 +11,19 @@ function enable() {
     StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['parcellite'] = 'parcellite';
     StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['transmission-gtk'] = 'transmission-gtk';
     StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['opera'] = 'opera';
+    StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['thunderbird-bin'] = 'thunderbird-bin';
+    StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['xchat'] = 'xchat';
 
     StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['vlc'] = 'vlc';
     StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['rhythmbox'] = 'rhythmbox';
     StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['deadbeef'] = 'deadbeef';
+    StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['banshee'] = 'banshee';
 
-	StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['radiotray'] = 'radiotray';
-	StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['shutter'] = 'shutter';
-	StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['instantbird'] = 'instantbird';
-	StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['clipit'] = 'clipit';
-	StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['dropbox'] = 'dropbox';
+    StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['radiotray'] = 'radiotray';
+    StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['shutter'] = 'shutter';
+    StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['instantbird'] = 'instantbird';
+    StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['clipit'] = 'clipit';
+    StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['dropbox'] = 'dropbox';
 }
 
 function disable() {
