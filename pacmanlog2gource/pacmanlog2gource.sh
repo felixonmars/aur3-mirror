eb0ec7d188908f18a4d1f74376de94400bbc3865
Thomas Dziedzic <gostrc@gmail.com>
2011-10-14 21:00:53 +0000

    updated on Fri Oct 14 21:00:53 UTC 2011

--- pacmanlog2gource/pacmanlog2gource.sh
+++ pacmanlog2gource/pacmanlog2gource.sh
@@ -41,21 +41,20 @@ timeend()
 	TDG=`calc $TEG - $TSG`
 }
 
-# start the time
+# start the timer
 timestart
-
 
 
 #check if we already have datadir, if not, create one
 if [ ! -d ${DATADIR} ] ; then
-	echo "No directory ${DATADIR} found, creating one."
+	echo -e "No directory \e[4;02m${DATADIR}\e[0m found, creating one."
 	mkdir ${DATADIR}
 fi
 
 # print the version into a file so we handle file formats being out of date properly later
 echo "0.8" >> ${DATADIR}/version
-if [[ `cat ${DATADIR}/version | grep -v "0.8"` ]] ; then
-	echo "Due to some slight changes in logfile generation, it is recommended to delete the files in ${DATADIR}/ and re-run this script."
+if [[ `cat ${DATADIR}/version | awk '! /0\.8/'` ]] ; then
+	echo -e "Due to some slight changes in logfile generation, it is recommended to delete the files in \e[4;02m${DATADIR}/\e[0m and re-run this script."
 	sleep 4
 fi
 
@@ -71,7 +70,7 @@ fi
 cp /var/log/pacman.log ${DATADIR}/pacman_tmp.log
 
 # we only want to proceed new entries, old ones are already included in the log
-diff -u ${DATADIR}/pacman_now.log /var/log/pacman.log | grep "^+" | sed -e 's/^+//' > ${DATADIR}/process.log
+diff -u ${DATADIR}/pacman_now.log /var/log/pacman.log | awk /'^+'/ | sed -e 's/^+//' > ${DATADIR}/process.log
 
 
 
@@ -84,8 +83,8 @@ diff -u ${DATADIR}/pacman_now.log /var/log/pacman.log | grep "^+" | sed -e 's/^+
 ORIGSIZE=`du ${DATADIR}/process.log | awk '{print $1}'`
 ORIGLINES=`cat ${DATADIR}/process.log | wc -l`
 
-echo "Purging pacman.log (${ORIGLINES} lines, ${ORIGSIZE}kB) and saving the result to ${DATADIR}."
-cat ${DATADIR}/process.log | sed -e 's/\[/\n[/g' | sed -e '/^$/d' | grep "]\ installed\|]\ upgraded\|]\ removed" > ${LOGTOBEPROCESSED}
+echo -e "Purging \e[4;02mpacman.log\e[0m (${ORIGLINES} lines, ${ORIGSIZE}kB) and saving the result to \e[4;02m${DATADIR}\e[0m."
+cat ${DATADIR}/process.log | sed -e 's/\[/\n[/g' -e '/^$/d' | awk '/] installed|] upgraded|] removed/' > ${LOGTOBEPROCESSED}
 
 PURGEDONESIZE=`du ${LOGTOBEPROCESSED} | awk '{print $1}'`
 PURGEDONELINES=`cat ${LOGTOBEPROCESSED} | wc -l`
@@ -104,21 +103,21 @@ while [ "$LINE" -le "$MAXLINES" ]; do
 
 # the line we are on
 	CURLINE=`cat ${LOGTOBEPROCESSED} | awk NR==${LINE}`
-# the date of the entry
-	DATE=`echo ${CURLINE} | grep -o "[0-9]\{4\}\-[0-9][0-9]\-[0-9]\{2\}\ [0-9]\{2\}\:[0-9]\{2\}"`
+# the date of the entry, get characters 2-17
+	DATE="${CURLINE:1:16}"
 # convert the date into unix time which can be read by gource
 	UDATE=`date +"%s" -d "${DATE}"`
 # find out if the package was installed, upgraded or removed
-	STATE=`echo ${CURLINE} | awk '{print $3}' | sed 's/installed/A/' | sed 's/upgraded/M/' | sed 's/removed/D/'`
+	STATE=`echo ${CURLINE} | awk '{print $3}' | sed  -e 's/installed/A/' -e 's/upgraded/M/' -e 's/removed/D/'`
 # get the actual package name
 	PKG=`echo ${CURLINE} | awk '{print $4}'`
 # add extensions to the package name
 # this way we can have the packages grouped and nicely colored in gource
 #
 # todo:  do things like:
-# first find everthing that contains an x, then, continue searching only these for xorg, xf86 etc and not everything
+# first find everything that contains an x, then, continue searching only these for xorg, xf86 etc and not everything
 #
-	if [[ `echo "${PKG}" | grep -o "lib" | grep -v "libreoffice"` == *lib* ]] ; then
+	if [[ "${PKG}" == *lib* ]] && [[ "${PKG}" != *libreoffice* ]] ; then
 		PKG=lib/${PKG}.lib
 	else
 		if [[ "${PKG}" == *xorg* ]] ; then
@@ -224,19 +223,17 @@ while [ "$LINE" -le "$MAXLINES" ]; do
 
 
 # here we print how log the script already took to run and try to estimate how log it will run until it finishes'
-# but we only update this every 500 lines to avoid unneccesary spamming
+# but we only update this every 500 lines to avoid unnecessary spamming
 # this will mostly be printed when initially obtaining the log
 	if [ "${LINEPERCOUT}" == "500" ] ; then
-
 		LINECOUNTCOOKIE=1
-
 		LINEPERC=`calc -p "${LINE} / ${MAXLINES} *100" | sed -e 's/\~//'`
-		LINEPERCOUT=`echo ${LINEPERC} | grep -o "[0-9]*\.\?[0-9]\?[0-9]" | head -n1`
 		timeend
-		TGDOUT=`echo ${TDG} | grep -o "[0-9]*\.\?[0-9]\?[0-9]" | head -n1`
-		TIMEDONEONE=`calc -p "100 / ${LINEPERC} *${TDG}" | sed 's/\~//'`
-		TIMEDONEFINAL=`calc -p "${TIMEDONEONE} - ${TDG}" | sed 's/\~//' | grep -o "[0-9]*\.\?[0-9]\?[0-9]" | head -n1`
-		echo "Already ${LINEPERCOUT}% done after ${TGDOUT}s."
+		# same as  echo ${TDG} | grep -o "[0-9]*\.\?[0-9]\?[0-9]" #  | head -n1
+		TGDOUT=`echo ${TDG} | awk 'match($0,/[0-9]*.?[0-9]?[0-9]/) {print substr($0,RSTART,RLENGTH)}'`
+		TIMEDONEONE=`calc -p "100 / ${LINEPERC:0:4} *${TDG}" | sed 's/\~//'`
+		TIMEDONEFINAL=`calc -p "${TIMEDONEONE} - ${TDG}" | sed 's/\~//' | awk 'match($0,/[0-9]*.?[0-9]?[0-9]/) {print substr($0,RSTART,RLENGTH)}'`
+		echo "Already ${LINEPERC:0:4}% done after ${TGDOUT}s."
 		echo -e "Done in approximately ${TIMEDONEFINAL}s.\n"
 		LINEPERCOUT=0
 	fi
@@ -244,7 +241,6 @@ while [ "$LINE" -le "$MAXLINES" ]; do
 # switch to next line and re-start the loop
 	LINE=`expr ${LINE} + 1`
 	LINEPERCOUT=`expr ${LINEPERCOUT} + 1`
-
 done
 
 
@@ -253,29 +249,30 @@ mv ${DATADIR}/pacman_tmp.log ${DATADIR}/pacman_now.log
 rm ${DATADIR}/pacman_purged.log ${DATADIR}/process.log
 
 # take the existing log and remove the paths so we have our pie-like log again which I had at the beginning of the developmen process of this script :)
-cat ${DATADIR}/pacman_gource_tree.log | sed 's/D|.*\//D\|/' | sed 's/M|.*\//M\|/' | sed 's/A|.*\//A\|/' > ${DATADIR}/pacman_gource_pie.log
+cat ${DATADIR}/pacman_gource_tree.log | sed -e 's/D|.*\//D\|/' -e 's/M|.*\//M\|/' -e 's/A|.*\//A\|/' > ${DATADIR}/pacman_gource_pie.log
 
 
 # how log did the script took to run?
 timeend
 
 if [[ ${LINECOUNTCOOKIE} == "1" ]] ; then
-	TIMEFINAL=`echo "${TDG}" | grep -o "[0-9]*\.\?[0-9]\?[0-9]" | head -n1`
+	TIMEFINAL=`echo ${TDG} | awk 'match($0,/[0-9]*.?[0-9]?[0-9]/) {print substr($0,RSTART,RLENGTH)}'`
 else
-	TIMEFINAL=`echo "${TDG}" | grep -o "[0-9]*\.\?[0-9]\{5\}" | head -n1`
+	TIMEFINAL=`echo ${TDG} | awk 'match($0,/[0-9]*.[0-9]{5}/) {print substr($0,RSTART,RLENGTH)}'`
 fi
 
 
-echo -e "100 % done after ${TIMEFINAL}s.\n"
+echo -e "100 % done after \e[1;31m${TIMEFINAL}\e[0ms.\n"
 
-echo "Output file is ./pacman_gource_tree.log"
+echo -e "Output file is \e[4;02m${DATADIR}/pacman_gource_tree.log\e[0m"
 
 # this is how we can visualize the log
-echo "If you have \"gource\" installed, run"
-echo -e "\t\e[3;32mgource ${DATADIR}/pacman_gource_tree.log -1200x720 -key --camera-mode overview --highlight-all-users -i 0 -a 0.001 -s 0.5 --hide progress,mouse --stop-at-end --max-files 99999999999 --max-file-lag 0.00001\e[0m"
+echo "If you have \"gource\" installed (should be, since it is set as dependency), run"
+echo -e "\t\e[3;32mgource \e[4;32m${DATADIR}/pacman_gource_tree.log\e[0;32m -1200x720 -key --camera-mode overview --highlight-all-users -i 0 -a 0.001 -s 0.5 --hide progress,mouse --stop-at-end --max-files 99999999999 --max-file-lag 0.00001\e[0m"
 echo -e "to visualize the log using gource.\n"
 echo "If you additionally want to make a video of the visualization and have the needed programs installed, append"
 echo -e "\t\e[3;32m--output-ppm-stream - | ffmpeg -f image2pipe -vcodec ppm -i - -vcodec libtheora -y -b 100000K -r 30 -threads 2 pacmanlog2gource_`date +%b\_%d\_%Y`.ogv\e[0m"
 echo "to the first command."
-echo  -e "\e[3;32mgource -H\e[0m will give you more information on how to use gource."
-echo "Alternatively, you can also replace \"tree\" with \"pie\" as source-logfile to get all packages in a pie-formation."
+echo -e "Alternatively, you can also replace \e[4;02m${DATADIR}/pacman_gource_tree.log\e[0m with \e[4;02m${DATADIR}/pacman_gource_pie.log\e[0m as source-logfile to get all packages in a pie-formation."
+echo -e "To skip the package names, replace \e[0;32m--hide progress,mouse\e[0m by \e[0;32m--hide progress,mouse,filenames\e[0m."
+echo -e "\e[3;32mgource -H\e[0m will give you more information on how to use gource."
