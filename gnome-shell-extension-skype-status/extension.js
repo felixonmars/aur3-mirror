d084bfbd4423601d12aeef613f3ebf82541daee6
Thomas Dziedzic <gostrc@gmail.com>
2011-10-20 06:31:30 +0000

    updated on Thu Oct 20 06:31:30 UTC 2011

--- gnome-shell-extension-skype-status/extension.js
+++ gnome-shell-extension-skype-status/extension.js
@@ -2,7 +2,15 @@
  
 const StatusIconDispatcher = imports.ui.statusIconDispatcher;
  
+function enable() {
+    StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['skype'] = 'skype';
+}
+
+function disable() {
+    StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['skype'] = '';
+}
+
 // gnome-shell extension entry point
 function init() {
-    StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['skype'] = 'skype';
+    //do nothing?..//
 }
