dde56b976a68fea9ada83f833d4054705f8d4497
Thomas Dziedzic <gostrc@gmail.com>
2011-11-03 07:00:58 +0000

    updated on Thu Nov  3 07:00:58 UTC 2011

---
+++ logitechmediaserver/rc.d
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+. /etc/rc.conf
+. /etc/rc.d/functions
+
+name=logitechmediaserver
+. /etc/conf.d/$name
+prog="cd $SERVER_HOME; ./slimserver.pl $SERVER_ARGS"
+
+PID=$(pgrep slimserver.pl)
+
+case "$1" in
+start)
+	stat_busy "Starting $name daemon"
+	[[ -z "$PID" ]] && eval $prog \
+	&& { add_daemon $name; stat_done; } \
+	|| { stat_fail; exit 1; }
+	;;
+stop)
+	stat_busy "Stopping $name daemon"
+	[[ -n "$PID" ]] && kill $PID &>/dev/null \
+	&& { rm_daemon $name; stat_done; } \
+	|| { stat_fail; exit 1; }
+	;;
+restart)
+	$0 stop
+	sleep 5
+	$0 start
+	;;
+*)
+	echo "usage: $0 {start|stop|restart|reload}"
+	exit 1
+	;;
+esac
