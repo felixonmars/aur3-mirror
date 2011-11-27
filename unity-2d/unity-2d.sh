c351389031a0d5ff3a18f5c76147348c538b68a7
Thomas Dziedzic <gostrc@gmail.com>
2011-11-27 01:31:02 +0000

    updated on Sun Nov 27 01:31:02 UTC 2011

---
+++ unity-2d/unity-2d.sh
@@ -0,0 +1,10 @@
+# This file is sourced by Xsession(5), not executed.
+# Add additionnal gconf path depending on selected desktop session.
+# The default profile is used otherwise.
+
+GCONF_PREFIX="/usr/share/gconf"
+if [ "x$DESKTOP_SESSION" = "xunity-2d" ]; then
+    export UNITY2D_DEFAULTS_PATH='xml:readonly:/etc/gconf/unity-2d.xml.defaults'
+    export UNITY2D_MANDATORY_PATH='xml:readonly:/etc/gconf/unity-2d.xml.mandatory'
+fi
+
