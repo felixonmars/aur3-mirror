2ce18e6cb007559cc7b5a99bb74d230552d3b95a
Thomas Dziedzic <gostrc@gmail.com>
2011-12-05 18:01:03 +0000

    updated on Mon Dec  5 18:01:03 UTC 2011

--- checksums/checksums.sh
+++ checksums/checksums.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Checksums v0.4
 # 
 # For original author please refer to:
 # http://kde-look.org/content/show.php/KonqCheckSum?content=83460
@@ -10,9 +11,9 @@ ERROR=0
 N=3
 
 if [ -f $1.md5 ]; then
-	refmd5=`cat $1.md5`
+	refmd5=`cat $1.md5 | awk '{ print $1;}'`
 	if [ "$md5" != "$refmd5" ]; then
-		kdialog --sorry "md5 doesn't match!"
+		kdialog --icon=security-low.png --sorry "md5 doesn't match!"
 		ERROR=1
 	fi
 else
@@ -20,9 +21,9 @@ else
 fi
 		
 if [ -f $1.sha1 ]; then
-	refsha1=`cat $1.sha1`
+	refsha1=`cat $1.sha1 | awk '{ print $1;}'`
 	if [ "$sha1" != "$refsha1" ]; then
-		kdialog --sorry "sha1 doesn't match!"
+		kdialog --icon=security-low.png --sorry "sha1 doesn't match!"
 		ERROR=1
 	fi
 else
@@ -30,9 +31,9 @@ else
 fi
 
 if [ -f $1.sha256 ]; then
-	refsha256=`cat $1.sha256`
+	refsha256=`cat $1.sha256 | awk '{ print $1;}'`
 	if [ "$sha256" != "$refsha256" ]; then
-		kdialog --sorry "sha256 doesn't match!"
+		kdialog --icon=security-low.png --sorry "sha256 doesn't match!"
 		ERROR=1
 	fi
 else
@@ -40,10 +41,10 @@ else
 fi
 
 if [ $N = 0 ]; then
-	kdialog --error "No checksum files exist for this file."
+	kdialog --icon=security-medium --error "No checksum files exist for this file."
 	ERROR=1
 fi
 
 if [ $ERROR = 0 ]; then
-	kdialog --msgbox "Checksums match."
+	kdialog --icon=security-high --msgbox "Checksums match."
 fi
