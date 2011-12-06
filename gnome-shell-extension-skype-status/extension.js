57c4c564cc40087a0c61f9eef58b231a8a1e8145
Thomas Dziedzic <gostrc@gmail.com>
2011-12-06 02:30:59 +0000

    updated on Tue Dec  6 02:30:59 UTC 2011

--- gnome-shell-extension-skype-status/extension.js
+++ gnome-shell-extension-skype-status/extension.js
@@ -1,8 +1,15 @@
 // Creates a system status notification icon for skype
  
 const StatusIconDispatcher = imports.ui.statusIconDispatcher;
- 
-// gnome-shell extension entry point
-function init() {
-    StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['skype'] = 'skype';
+
+function enable() {
+	StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['skype'] = 'skype';
 }
+
+function disable() {
+	StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['skype'] = '';
+}
+
+function init() {
+}
+
