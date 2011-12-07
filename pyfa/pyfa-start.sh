0d534db2ff2bcca03e720eae2abdd8109e23d475
Thomas Dziedzic <gostrc@gmail.com>
2011-12-07 23:00:59 +0000

    updated on Wed Dec  7 23:00:59 UTC 2011

--- pyfa/pyfa-start.sh
+++ pyfa/pyfa-start.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-# Download data files
-[ -f /usr/share/pyfa/downloadData.py ] && python /usr/share/pyfa/downloadData.py
-
 # Launch pyfa
-python2 /usr/share/pyfa/launch.py
+python2 /usr/share/pyfa/pyfa.py
