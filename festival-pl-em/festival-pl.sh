801f38f112dca9a6a975b4e98016f8baba803eb3
Thomas Dziedzic <gostrc@gmail.com>
2012-02-05 18:14:53 +0000

    updated on Sun Feb  5 18:14:52 UTC 2012

--- festival-pl-em/festival-pl.sh
+++ festival-pl-em/festival-pl.sh
@@ -1,52 +1,144 @@
 #!/bin/bash
 
-VERSION="0.1"
+TRANSLATE='
+s|*| gwiazdka |g
+s|/| slesz |g
+s|\\| bekslesz |g
+s|-| minus |g
+s|+| plus |g
+s|(|w nawiasie: |g
+s|)| koniec nawiasu|g
+s|\[|w nawiasie kawu: |g
+s|\]| koniec nawiasu|g
+s|{|w klamrze: |g
+s|}| koniec klamry|g
+s|ó|u|g
+s|Ó|U|g
+s|ą|o~|g
+s|Ą|O~|g
+s|ć|c~|g
+s|Ć|C~|g
+s|ę|e~|g
+s|Ę|E~|g
+s|ł|l/|g
+s|Ł|L/|g
+s|ń|n~|g
+s|Ń|N~|g
+s|ś|s~|g
+s|Ś|S~|g
+s|ź|z~|g
+s|Ż|Z~|g
+s|ż|z*|g
+s|Ź|Z*|g
+'
 
-print_help() {
-printf "\e[1mfestival-pl\e[0m (konwerter czcionek) - wersja \e[1m%s\e[0m (c) \e[1m3ED 2009\e[0m GPL3
+test -f "$HOME/.festival-pl.rc" && . "$HOME/.festival-pl.rc"
 
-\e[1mUżycie:\e[0m festival-pl -h|--help|--pomoc
-        festival-pl \"Wpisz tutaj swój tekst\"
+VERSION="0.2"
+YEARS="2009, 2012"
 
-\e[1mFESTIVAL:\e[0m
-\e[1m1.\e[0m Aby ustawić polski głos jako domyślny:
-     \e[1ma)\e[0m edytuj plik: /usr/share/festival/voices.scm
-     \e[1mb)\e[0m wyszukaj linijkę: defvar default-voice-priority-list
-     \e[1mc)\e[0m do listy dopisz: cstr_pl_em_diphone
-\e[1m2.\e[0m Aby korzystać z festival bez użycia tego front-endu, pamiętaj że
-   Festival (albo polski głos) nie obsługuje poslkich czcionek.. Spis:
-     ą - o~ - oun
-     ć - c~ - tsi
-     ę - e~ - eun
-     ł - l/ - eu
-     ń - n~ - eni
-     ś - s~ - esi
-     ź - z~ - ziet
-     ż - z* - rzet
-     ó - u  - u
+usage() {
+cat <<EOF
+festival-pl (konwerter) - wersja $VERSION (c) 3ED $YEARS GPL3
 
-\e[1mMoja uwaga:\e[0m Głos cstr_pl_em_diphone nie jest najwyższych lotów ale nie
-            znam alternatywy.. :( Jest on wydany jako darmowy w zastoso-
-            waniu niekomercyjnym, program \e[4mfestival-pl\e[0m jest wydany jako
-            program wolnego/otwartego oprogramowania na licencji GPL3.
+Użycie:
+  festival-pl [-h|--help|--help-festival|--help-translate]
+  festival-pl [--translate] "Łąka zimną porą"
+  echo "Łąka zimną porą" | festival-pl [--translate]
 
-\e[1mWięcej na temat cstr_pl_em_diphone znajdziesz tutaj:\e[0m
+Opcje:
+  --translate       poprawiony tekst zostanie wypisany na
+                    stdout i nie zostanie poddany syntezie
+  --help-festival   pomoc na temat konfiguracji festival
+  --help-translate  pomoc na temat modyfikacji bazy translate
+  --help, -h        ten ekran pomocy
+
+Licancja:
+  Moduł głosu wydany jest jako darmowy dla niekomercyjnych zastosowań.
+  Ten skrypt (\`festival-pl') jest objęty zasadami licencji GPL3.
+
+Więcej na temat cstr_pl_em_diphone (głosu) znajdziesz tutaj:
   /usr/share/festival/voices/polish/cstr_pl_em_diphone/README
   /usr/share/festival/voices/polish/cstr_pl_em_diphone/przeczytajto.txt
-" "$VERSION"
+EOF
+exit
 }
 
-case $1 in
-	-h|--help|--pomoc) print_help;;
-	*) echo "$@" | sed '
-s|ó|u|g
-s|ą|o~|g
-s|ć|c~|g
-s|ę|e~|g
-s|ł|l/|g
-s|ń|n~|g
-s|ś|s~|g
-s|ź|z~|g
-s|ż|z*|g
-' | festival --tts
-esac
+usage_translate() {
+  cat >&2 <<EOF
+--------------------------------------------------------------------
+Baza \`translate' to zmienna w formacie wyrażeń regularnych programu
+sed (powinna być w apostrofach), który podmienia znaki na takie,
+które \`festival' potrafi rozpoznać. Możesz utworzyć bliźniaczą
+bazę w swoim katalogu domowym wykonująć polecenie:
+
+    festival-pl --help-translate > ~/.festival-pl.rc
+--------------------------------------------------------------------
+
+EOF
+  cat <<EOF
+#
+# To jest obecnie używana baza \`TRANSLATE' dla \`festival-pl'
+#
+TRANSLATE='$TRANSLATE'
+EOF
+exit
+}
+usage_festival() {
+  cat <<EOF
+Aby ustawić polski głos jako domyślny:
+  a) edytuj plik: /usr/share/festival/voices.scm
+  b) wyszukaj linijkę: defvar default-voice-priority-list
+  c) do listy dopisz: cstr_pl_em_diphone
+
+Przykład:
+...
+ (defvar default-voice-priority-list 
+-  '(kal_diphone
++  '(cstr_pl_em_diphone
++    kal_diphone
+     cmu_us_bdl_arctic_hts
+...
+EOF
+exit
+}
+
+error() {
+  test -n "$1" && printf "\e[31;1mBŁĄÐ:\e[0m %s\n" "$1"
+  exit 1
+}
+
+translate() {
+  echo "$SAYTEXT" | sed "$TRANSLATE"
+  test "$?" -gt "0" && error "Baza translate nie jest poprawna.."
+}
+saytext() {
+  cat | festival --tts
+  test "$?" -gt "0" && error "Program festival zakończył błędem.."
+}
+
+ONLY_TRANSLATE="0"
+SAYTEXT=""
+
+until [ "$#" -le "0" ]; do
+  case "$1" in
+	  -h|--help) usage;;
+    --help-festival) usage_festival;;
+    --help-translate) usage_translate;;
+    --translate) ONLY_TRANSLATE="1";;
+  	*) SAYTEXT+="$1 ";;
+  esac
+  shift
+done
+
+#if [ "`uname -s`" = "Linux" ]; then
+  test -e "/dev/dsp" || error "Brak pliku: \`/dev/dsp', załaduj moduł: \`snd_pcm_oss'"
+#fi
+
+test -n "$SAYTEXT" || SAYTEXT="`cat`"
+
+if [ "$ONLY_TRANSLATE" = "1" ]; then
+  translate
+else
+  translate | saytext
+fi
