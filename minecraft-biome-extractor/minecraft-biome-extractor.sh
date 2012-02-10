012387d5fab59d4aca87b5ec9f7cb684b601a4d2
Thomas Dziedzic <gostrc@gmail.com>
2012-02-10 05:00:32 +0000

    updated on Fri Feb 10 05:00:31 UTC 2012

--- minecraft-biome-extractor/minecraft-biome-extractor.sh
+++ minecraft-biome-extractor/minecraft-biome-extractor.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar /opt/minecraft-biome-extractor/MinecraftBiomeExtractor.jar -jar /opt/minecraft-biome-extractor/minecraft.jar "$@"
+java -jar /opt/minecraft-biome-extractor/MinecraftBiomeExtractor.jar "$@"
