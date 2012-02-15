36bce389462d2cdce68b926452e0b06055e1cd6e
Thomas Dziedzic <gostrc@gmail.com>
2012-02-15 11:02:45 +0000

    updated on Wed Feb 15 11:02:44 UTC 2012

--- nwn/rc.nwserver.sh
+++ nwn/rc.nwserver.sh
@@ -12,7 +12,7 @@ NWSERVER_ARGS=""
 
 _daemonName="Neverwinter Nights server"
 _logDir="/var/log/nwserver"
-_runDir="/var/run/nwserver"
+_runDir="/run/nwserver"
 _srvDir="/srv/nwn"
 _expectScript=$(cat << EOS
 set timeout "$NWSERVER_SAVE_TIMEOUT"
@@ -40,11 +40,21 @@ case "$1" in
   start)
     stat_busy "Starting $_daemonName"
     if [[ ! -f "$_runDir/server.pid" ]]; then
+      cd "$_srvDir"
       if [[ ! -d "$_runDir" ]]; then
         mkdir -p "$_runDir"
         chown nwserver:nwserver "$_runDir"
       fi
-      su nwserver -c "RW_BRANCH=$_srvDir detachtty --dribble-file $_logDir/server_stdout.log --log-file $_logDir/detachtty.log --pid-file $_runDir/server.pid $_runDir/socket /usr/bin/nwserver $NWSERVER_ARGS -interactive" &&
+      su nwserver -c "RW_BRANCH=$_srvDir detachtty --dribble-file $_logDir/server_stdout.log  \
+                                                   --log-file $_logDir/detachtty.log          \
+                                                   --pid-file $_runDir/server.pid             \
+                                                   $_runDir/socket /usr/bin/nwserver $NWSERVER_ARGS -interactive" &&
+      chmod 770 "$_runDir/socket" &&
+      chmod 644 "$_runDir/server.pid" &&
+      chown nwserver:log "$_logDir/server_stdout.log" &&
+      chown nwserver:log "$_logDir/detachtty.log" &&
+      chmod 640 "$_logDir/server_stdout.log" &&
+      chmod 640 "$_logDir/detachtty.log" &&
       add_daemon nwserver &&
       stat_done || stat_fail
     else
@@ -77,11 +87,14 @@ case "$1" in
     sleep 1
     $0 start
     ;;
+  attach)
+    attachtty "$_runDir/socket"
+    ;;
   status)
     stat_busy "Checking $_daemonName status"
     ck_status nwserver
     ;;
   *)
-    echo "usage: $0 {start|stop|restart|status}"
+    echo "usage: $0 {start|stop|restart|attach|status}"
 esac
 exit 0
