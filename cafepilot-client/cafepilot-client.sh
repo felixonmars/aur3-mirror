f8912eb6c55d374e90b36e76eb6f4b80d527a1ba
Thomas Dziedzic <gostrc@gmail.com>
2012-03-07 10:07:10 +0000

    updated on Wed Mar  7 10:07:08 UTC 2012

---
+++ cafepilot-client/cafepilot-client.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -classpath /opt/CafePilot_Client/ -jar /opt/CafePilot_Client/CafePilot_Client.jar
---
+++ cafepilot-srm/cafepilot-srm.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -classpath /opt/CafePilot_Service_Request_Monitor/ -jar /opt/CafePilot_Service_Request_Monitor/CafePilot_Service_Request_Monitor.jar
