5e7408c0f09930f7724ef12a7f4a8944ea636047
Thomas Dziedzic <gostrc@gmail.com>
2012-03-15 06:00:12 +0000

    updated on Thu Mar 15 06:00:12 UTC 2012

--- chromium-dev/chromium-dev.sh
+++ chromium-dev/chromium-dev.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+if [ -f /etc/chromium-dev/default ] ; then
+  . /etc/chromium-dev/default
+fi
+#!/bin/sh
 export CHROME_WRAPPER=/usr/lib/chromium-dev/chromium
 export CHROME_DESKTOP=chromium-dev.desktop
-exec /usr/lib/chromium-dev/chromium --class=chromium-dev "$@"
+exec /usr/lib/chromium-dev/chromium --class=chromium-dev $CHROMIUM_FLAGS $@
