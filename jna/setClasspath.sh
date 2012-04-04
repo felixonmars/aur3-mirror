e072aeed7afd0af0ecb4e6659c28abb9cff203bc
Thomas Dziedzic <gostrc@gmail.com>
2012-04-04 21:00:12 +0000

    updated on Wed Apr  4 21:00:12 UTC 2012

--- jna/setClasspath.sh
+++ jna/setClasspath.sh
@@ -4,5 +4,4 @@
 # This script will set JNA's classpath variables.
 ##
 
-JNA_CLASSPATH=/usr/share/java/jna/jna.jar
-JNA_PLATFORM_CLASSPATH=/usr/share/java/jna/platform.jar:$JNA_CLASSPATH
+JNA_CLASSPATH=/usr/share/java/jna/platform.jar:/usr/share/java/jna/jna.jar
