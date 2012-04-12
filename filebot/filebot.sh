a341cd01ad708e366de224ee362e3bc3d40a5df2
Thomas Dziedzic <gostrc@gmail.com>
2012-04-12 13:00:14 +0000

    updated on Thu Apr 12 13:00:14 UTC 2012

--- filebot/filebot.sh
+++ filebot/filebot.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Dapplication.deployment=deb -Djna.library.path=/usr/share/java/filebot -Xmx256m -jar /usr/share/java/filebot/filebot.jar "$@"
+java -Dapplication.deployment=aur -Djna.library.path=/usr/share/java/filebot -Xmx256m -jar /usr/share/java/filebot/filebot.jar "$@"
