5cd0b89036adb3fc2723b28e2f234fabe4169901
Thomas Dziedzic <gostrc@gmail.com>
2012-04-22 01:16:58 +0000

    updated on Sun Apr 22 01:16:54 UTC 2012

--- botanicula/botanicula.sh
+++ botanicula/botanicula.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/opt/adobe-air-sdk/bin/adl -nodebug /usr/share/games/botanicula/META-INF/AIR/application.xml /usr/share/games/botanicula &
+/opt/adobe-air-sdk/bin/adl -nodebug /usr/share/botanicula/META-INF/AIR/application.xml /usr/share/botanicula &
