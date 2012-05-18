a9f8b57a63c84e23cc5bb47a7bd7f23e8f78fe03
Thomas Dziedzic <gostrc@gmail.com>
2012-05-18 05:00:25 +0000

    updated on Fri May 18 05:00:25 UTC 2012

--- sudokuki/sudokuki.sh
+++ sudokuki/sudokuki.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd /usr/share/sudokuki
-"$JAVA_HOME/bin/java" -Djava.library.path=/usr/lib -jar sudokuki-1.1.4.jar -ui Swing "$@"
+"$JAVA_HOME/bin/java" -Djava.library.path=/usr/lib -jar sudokuki.jar -ui Swing "$@"
