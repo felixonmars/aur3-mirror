1fd368cc2d389ef94d427c81f214893f056ff8fd
Thomas Dziedzic <gostrc@gmail.com>
2012-05-18 07:00:15 +0000

    updated on Fri May 18 07:00:15 UTC 2012

--- fly8/fly8.sh
+++ fly8/fly8.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-cd /opt/fly8
-./fly8 L$HOME/.fly8.log "$@"
+# Copy configuration file to the home directory
+if [[ ! -f $HOME/.fly8.ini ]]; then
+  cp /usr/share/fly8/fly.ini $HOME/.fly8.ini
+fi
+
+# Launch Fly8
+fly8bin F/usr/share/fly8 I$HOME/.fly8.ini L$HOME/.fly8.log "$@"
