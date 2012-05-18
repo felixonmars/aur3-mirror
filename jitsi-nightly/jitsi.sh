ee2e134d2c4a965bfdc25b806b28a06df9b6b427
Thomas Dziedzic <gostrc@gmail.com>
2012-05-18 11:01:12 +0000

    updated on Fri May 18 11:01:11 UTC 2012

--- jitsi-nightly/jitsi.sh
+++ jitsi-nightly/jitsi.sh
@@ -1,20 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 CLIENTARGS=""
-arch | grep i686 && CLIENTARGS="-client -Xmx256m"
+uname -m | grep i686 && CLIENTARGS="-client -Xmx256m"
 
-javabin=${JAVA_HOME}/bin/java
+javabin="${JAVA_HOME}/bin/java"
 
-SCDIR=/usr/lib/jitsi
-LIBPATH=$SCDIR/lib
-CLASSPATH=$LIBPATH/jdic_stub.jar:$LIBPATH/jdic-all.jar:$LIBPATH/felix.jar:$LIBPATH/bcprovider.jar:$SCDIR/sc-bundles/sc-launcher.jar:$SCDIR/sc-bundles/util.jar
-FELIX_CONFIG=$LIBPATH/felix.client.run.properties
-LOG_CONFIG=$LIBPATH/logging.properties
-COMMAND="$javabin $CLIENTARGS -classpath $CLASSPATH -Djna.library.path=$LIBPATH/native -Dfelix.config.properties=file:$FELIX_CONFIG -Djava.util.logging.config.file=$LOG_CONFIG net.java.sip.communicator.launcher.SIPCommunicator"
+SCDIR="/usr/lib/jitsi"
+LIBPATH="${SCDIR}/lib"
+CLASSPATH="${LIBPATH}/jdic_stub.jar:${LIBPATH}/jdic-all.jar:${LIBPATH}/felix.jar:${LIBPATH}/bcprovider.jar:${SCDIR}/sc-bundles/sc-launcher.jar:${SCDIR}/sc-bundles/util.jar"
+FELIX_CONFIG="${LIBPATH}/felix.client.run.properties"
+LOG_CONFIG="${LIBPATH}/logging.properties"
+COMMAND="${javabin} ${CLIENTARGS} -classpath ${CLASSPATH} -Djna.library.path=${LIBPATH}/native -Dfelix.config.properties=file:${FELIX_CONFIG} -Djava.util.logging.config.file=${LOG_CONFIG} net.java.sip.communicator.launcher.SIPCommunicator"
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$LIBPATH/native
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${LIBPATH}/native"
 
-cd $SCDIR
+cd "${SCDIR}"
 
-exec $COMMAND $*
+exec ${COMMAND} $*
 
