8877fd89f6fbcbcc24367313b0309bfeefceffae
Thomas Dziedzic <gostrc@gmail.com>
2012-05-24 15:02:08 +0000

    updated on Thu May 24 15:02:07 UTC 2012

--- xcrysden-bin/xcrysden.sh
+++ xcrysden-bin/xcrysden.sh
@@ -1,3 +1,3 @@
-export XCRYSDEN_TOPDIR=/opt/XCrySDen-1.5.24-bin-shared
+export XCRYSDEN_TOPDIR=/opt/xcrysden-1.5.53-bin-shared
 export XCRYSDEN_SCRATCH=/tmp
 export PATH="$XCRYSDEN_TOPDIR:$PATH:$XCRYSDEN_TOPDIR/scripts:$XCRYSDEN_TOPDIR/util"
