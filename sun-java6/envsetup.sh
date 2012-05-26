b1c8908fe0fce4b3c543b6e9ff83bd7efc28e036
Thomas Dziedzic <gostrc@gmail.com>
2012-05-26 12:00:16 +0000

    updated on Sat May 26 12:00:15 UTC 2012

---
+++ sun-java6/envsetup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export J2SDKDIR=/opt/sun-java6
+export J2REDIR=/opt/sun-java6/jre
+export JAVA_HOME=/opt/sun-java6
+export DERBY_HOME=/opt/sun-java6/db
+export PATH=/opt/sun-java6/bin:/opt/sun-java6/jre/bin:/opt/sun-java6/db/bin:$PATH
