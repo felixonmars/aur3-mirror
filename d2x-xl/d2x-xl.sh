229bf01febab67d4bfa5ba7c6a813b604ae006aa
Thomas Dziedzic <gostrc@gmail.com>
2012-06-13 18:00:24 +0000

    updated on Wed Jun 13 18:00:24 UTC 2012

--- d2x-xl/d2x-xl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ ! -d "$HOME/.d2x-xl" ]; then
-  mkdir -p "$HOME/.d2x-xl"
-  7za x -y "/usr/share/d2x-xl/d2x-xl-data.7z" -o"$HOME/.d2x-xl"
-  cp "$HOME/.d2x-xl/data/descent.tex.eng" "$HOME/.d2x-xl/data/descent.tex"
-fi
-
-/usr/share/d2x-xl/d2x-xl
