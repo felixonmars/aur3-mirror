1decfab4cb4e4960b922212fbecf51fb2db2e5bc
Thomas Dziedzic <gostrc@gmail.com>
2012-06-24 07:00:58 +0000

    updated on Sun Jun 24 07:00:58 UTC 2012

--- tn5250j/tn5250j.sh
+++ tn5250j/tn5250j.sh
@@ -1,12 +1,3 @@
 #! /bin/sh
-
-# Runs the tn5250j 5250 java emulator
-
-# Sets the tn5250j home directory
 TN5250J_HOME='/usr/share/java/tn5250j/'
-
-# Change to the emulator's directory
-cd ${TN5250J_HOME}/
-
-# Does the effective launching
-java -jar tn5250j.jar
+java -jar ${TN5250J_HOME}/tn5250j.jar
