d4dc6159c1a8774cd5d2db4181da382cfe38f233
Thomas Dziedzic <gostrc@gmail.com>
2012-06-27 21:00:47 +0000

    updated on Wed Jun 27 21:00:47 UTC 2012

--- palm-novacom-git/novacomd-conf.d
+++ palm-novacom-git/novacomd-conf.d
@@ -1,1 +1,14 @@
+# /etc/conf.d/novacomd
+
+# Usage:  novacomd [option]...
+# options:
+# -b                  bind ipaddress for remote access, default is 127.0.0.1 
+# -t <seconds>        set recovery timeout interval, valid range 2 ~ 5, default is 5
+# -c <host>:<port>    connect to specified IP address
+# -e <io-retry-timeout>    timeout to retry packet I/O in milliesecond
+# -s <io-retry-delay>    delay to retry packet I/O in milliesecond
+# -d                  run in the background
+# -V                  print version info
+# -h                  display this help
+
 NOVACOMD_OPTIONS=""
--- palm-novacom-git/novacomd-rc.d
+++ palm-novacom-git/novacomd-rc.d
@@ -1,49 +1,93 @@
 #!/bin/bash
+# /etc/rc.d/novacomd
+# Arch Linux startup script for novacomd
 
-# source application-specific settings
-[ -f /etc/conf.d/novacomd ] && . /etc/conf.d/novacomd
+# begin novacomd specific code
+[[ -f /etc/conf.d/novacomd ]] && . /etc/conf.d/novacomd
+
+NAME="novacomd"
+BIN="/usr/bin/${NAME}"
+DAEMON_OPTIONS="-d"
+
+start_cmd(){
+  ${BIN} ${DAEMON_OPTIONS} ${NOVACOMD_OPTIONS} &> /dev/null
+}
+
+stop_cmd(){
+  kill ${PID}
+}
+# end novacomd specific code
 
 . /etc/rc.conf
 . /etc/rc.d/functions
 
-PID=`pidof -o %PPID /opt/Palm/novacom/novacomd`
-
 start() {
-  stat_busy "Starting Palm Novacom service"
-  export LD_LIBRARY_PATH=/opt/Palm/novacom 
-  [ -z "$PID" ] && exec /opt/Palm/novacom/novacomd $NOVACOMD_OPTIONS &>/dev/null &
-  if [ $? -gt 0 ]; then
-    stat_fail
+  PID=$(get_pid ${NAME})
+  if [[ -z "${PID}" ]]; then
+    if ck_daemon ${NAME}; then
+      stat_busy "starting ${NAME}"
+      sleep 0.5
+      start_cmd
+      if [[ ${?} -gt 0 ]]; then
+        stat_busy "could not start ${NAME}"
+        stat_fail && return 1
+      else
+        add_daemon ${NAME} && stat_done
+      fi
+    else
+      stat_busy "${NAME} is dead but the run file exists"
+      stat_fail && return 1
+    fi
   else
-    add_daemon novacomd
-    stat_done
+    stat_busy "${NAME} is already running (pid ${PID})"
+    stat_fail && return 1
   fi
 }
 
 stop() {
-  stat_busy "Stopping Palm Novacom service"
-  [ ! -z "$PID" ]  && kill $PID
-  if [ $? -gt 0 ]; then
-    stat_fail
+  PID=$(get_pid ${NAME})
+  if [[ -n "${PID}" ]]; then
+    if ! ck_daemon ${NAME}; then
+      stat_busy "stopping ${NAME}"
+      sleep 0.5
+      stop_cmd
+      if [[ ${?} -gt 0 ]]; then
+        stat_busy "could not stop ${NAME}"
+        stat_fail && return 1
+      else
+        rm_daemon ${NAME} && stat_done
+      fi
+    else
+      stat_busy "${NAME} is running but no run file exists"
+      stat_fail && return 1
+    fi
   else
-    rm_daemon novacomd
-    stat_done
+    stat_busy "${NAME} is not running"
+    stat_fail && return 1
   fi
 }
 
-case "$1" in
+status() {
+  PID=$(get_pid ${NAME})
+  if [[ -n "${PID}" ]]; then
+    stat_busy "${NAME} is running (pid ${PID})"
+  else
+    stat_busy "${NAME} is not running"
+  fi
+  ck_status ${NAME}
+}
+
+case "${1}" in
   start)
-    start
-    ;;
+    start;;
   stop)
-    stop
-    ;;
+    stop;;
   restart)
-    # calling 'stop' and 'start' without the $0 fails...
-    $0 stop
-    $0 start
-    ;;
+    stop; sleep 0.5; start;;
+  status)
+    status;;
   *)
-    echo "usage: $0 {start|stop|restart}"
+    echo "usage: ${0} {start|stop|restart|status}"
 esac
+
 exit 0
