3391d6cde797574ce8706b9a7e747ac99246373c
Thomas Dziedzic <gostrc@gmail.com>
2012-07-27 10:06:12 +0000

    updated on Fri Jul 27 10:06:12 UTC 2012

--- serviio/serviio-console.sh
+++ serviio/serviio-console.sh
@@ -4,7 +4,7 @@ if [ $UID -eq 0 ]
 then
     SERVIIO_CONSOLE_HOME="/usr/share/java/serviio"
     
-    CP="$SERVIIO_CONSOLE_HOME/lib/serviio-client.jar:$SERVIIO_CONSOLE_HOME/lib/serviio.jar:$SERVIIO_CONSOLE_HOME/lib/log4j.jar:$SERVIIO_CONSOLE_HOME/lib/slf4j-api.jar:$SERVIIO_CONSOLE_HOME/lib/slf4j-log4j12.jar:$SERVIIO_CONSOLE_HOME/lib/jul-to-slf4j.jar:$SERVIIO_CONSOLE_HOME/lib/org.restlet.jar:$SERVIIO_CONSOLE_HOME/lib/org.restlet.ext.xstream.jar:$SERVIIO_CONSOLE_HOME/lib/xstream.jar:$SERVIIO_CONSOLE_HOME/config"
+    CP="$SERVIIO_CONSOLE_HOME/lib/*:$SERVIIO_CONSOLE_HOME/config"
     
     JAVA_OPTS="-Dserviio.home=$SERVIIO_CONSOLE_HOME -Djava.net.preferIPv4Stack=true"
     
--- serviio/serviio.sh
+++ serviio/serviio.sh
@@ -4,7 +4,7 @@ if [ $UID -eq 0 ]
 then
     SERVIIO_HOME="/usr/share/java/serviio"
     
-    CP="$SERVIIO_HOME/lib/serviio.jar:$SERVIIO_HOME/lib/derby.jar:$SERVIIO_HOME/lib/jcs.jar:$SERVIIO_HOME/lib/concurrent.jar:$SERVIIO_HOME/lib/freemarker.jar:$SERVIIO_HOME/lib/httpcore.jar:$SERVIIO_HOME/lib/jaudiotagger.jar:$SERVIIO_HOME/lib/jul-to-slf4j.jar:$SERVIIO_HOME/lib/jcl-over-slf4j.jar:$SERVIIO_HOME/lib/log4j.jar:$SERVIIO_HOME/lib/sanselan.jar:$SERVIIO_HOME/lib/slf4j-api.jar:$SERVIIO_HOME/lib/slf4j-log4j12.jar:$SERVIIO_HOME/lib/org.restlet.jar:$SERVIIO_HOME/lib/org.restlet.ext.xstream.jar:$SERVIIO_HOME/lib/xstream.jar:$SERVIIO_HOME/lib/rome.jar:$SERVIIO_HOME/lib/rome-modules.jar:$SERVIIO_HOME/lib/jdom.jar:$SERVIIO_HOME/lib/groovy-all.jar:$SERVIIO_HOME/lib/winp.jar:$SERVIIO_HOME/lib/org.restlet.ext.gson.jar:$SERVIIO_HOME/lib/gson.jar:$SERVIIO_HOME/config"
+    CP="$SERVIIO_HOME/lib/*:$SERVIIO_HOME/config"
     
     JAVA_OPTS="-Djava.net.preferIPv4Stack=true -Djava.awt.headless=true -Dderby.system.home=$SERVIIO_HOME/library -Dserviio.home=$SERVIIO_HOME"
     
