82bfeacdb2f4bcfe487a390f8a96c1c833f7189e
Thomas Dziedzic <gostrc@gmail.com>
2012-08-02 13:00:15 +0000

    updated on Thu Aug  2 13:00:14 UTC 2012

--- unvanquished/unvanquished.sh
+++ unvanquished/unvanquished.sh
@@ -1,3 +1,16 @@
-#! /bin/sh
-cd /opt/unvanquished
-exec ./unvanquished +set fs_libpath $PWD +set fs_basepath $PWD $@
+#!/bin/sh
+# launcher script for unvanquished
+# handles unv:// URI's as first parameter securely
+
+if echo "$1" | grep -q '^unv://'; then
+	uri=`echo "$1" | grep -o '^unv://[^[:space:]+;]*'`
+
+	if [ -p ~/.Unvanquished/main/pipe ]; then
+		echo "connect $uri" > ~/.Unvanquished/main/pipe
+	else
+		$0 +connect $uri
+	fi
+else
+	cd /opt/unvanquished
+	exec ./unvanquished +set fs_libpath $PWD +set fs_basepath $PWD $@
+fi
