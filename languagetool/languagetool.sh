42524414225c7f02c1fdd7d1e304dc11ecadae57
Thomas Dziedzic <gostrc@gmail.com>
2012-08-22 10:00:30 +0000

    updated on Wed Aug 22 10:00:30 UTC 2012

--- languagetool/languagetool.sh
+++ languagetool/languagetool.sh
@@ -1,39 +1,38 @@
 #!/bin/bash
 
-file_given=false
+file_present=false
 asked_for_help=false
 
-for arg in "$@"
-do
-  if [[ $arg != -* ]]
-  then
-    file_given=true
+for arg in "$@"; do
+  if [[ $arg != -* ]]; then
+    file_present=true
   fi
-  if [[ $arg == -h ]]
-  then
+  if [[ $arg == -h ]]; then
     asked_for_help=true
   fi
-  if [[ $arg == --help ]]
-  then
+  if [[ $arg == --help ]]; then
     asked_for_help=true
   fi
 done
 
-CLI_command=/usr/share/languagetool/LanguageTool.jar
-GUI_command=/usr/share/languagetool/LanguageToolGUI.jar
+CP=/usr/share/languagetool
+for name in /usr/share/java/languagetool/*.jar ; do
+  CP=$CP:$name
+done
+CLI_command='org.languagetool.Main'
+GUI_command='org.languagetool.gui.Main'
 
-if $asked_for_help
-then
+if $asked_for_help; then
   echo "Command-line interface (CLI) help:"
-  java -jar $CLI_command -h | sed "s/java de.danielnaber.languagetool.Main/languagetool/"
+  "$JAVA_HOME/bin/java" -cp $CP $CLI_command -h | sed "s/java -jar LanguageTool.jar/languagetool/"
+  echo
   echo "Graphical user interface (GUI) help:"
-  java -jar $GUI_command -h | sed "s/java de.danielnaber.languagetool.gui.Main/languagetool/"
+  "$JAVA_HOME/bin/java" -cp $CP $GUI_command -h | sed "s/java org.languagetool.gui.Main/languagetool/"
 else
-  if $file_given
-  then
-    java -jar $CLI_command "$@"
+  if $file_present; then
+    "$JAVA_HOME/bin/java" -cp $CP $CLI_command "$@"
   else
-    java -jar $GUI_command "$@"
+    "$JAVA_HOME/bin/java" -cp $CP $GUI_command "$@"
   fi
 fi
 
