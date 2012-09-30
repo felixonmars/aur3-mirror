ab90b2e265cdb26888a8a106ce17fa95c278d4ec
Thomas Dziedzic <gostrc@gmail.com>
2012-09-30 15:00:15 +0000

    updated on Sun Sep 30 15:00:15 UTC 2012

--- iceweasel/vendor.js
+++ iceweasel/vendor.js
@@ -7,4 +7,3 @@ pref("browser.shell.checkDefaultBrowser", false);
 // Don't disable our bundled extensions in the application directory
 pref("extensions.autoDisableScopes", 11);
 pref("extensions.shownSelectionUI", true);
- 
