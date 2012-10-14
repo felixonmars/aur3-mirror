3cad435761b807069016a650c16123e323728226
Thomas Dziedzic <gostrc@gmail.com>
2012-10-14 20:00:15 +0000

    updated on Sun Oct 14 20:00:15 UTC 2012

--- seamonkey-kde/vendor.js
+++ seamonkey-kde/vendor.js
@@ -1,20 +1,3 @@
-// Use LANG environment variable to choose locale
-pref("intl.locale.matchOS", true);
-
-// Disable default browser checking.
-pref("browser.shell.checkDefaultBrowser", false);
-
-// Disable auto-update
-pref("app.update.enabled", false);
-
 // Don't disable our bundled extensions in the application directory
 pref("extensions.autoDisableScopes", 11);
 pref("extensions.shownSelectionUI", true);
-
-// Other custom settings
-pref("browser.display.use_system_colors", true);
-pref("general.smoothScroll", true);
-pref("accessibility.typeaheadfind.soundURL", none);
-pref("browser.download.manager.scanWhenDone", false);
-pref("browser.showQuitWarning", true);
-pref("browser.startup.homepage_override.mstone", "ignore");
