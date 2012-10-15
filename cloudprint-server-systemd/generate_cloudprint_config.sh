ff1efcc2dbd65240f6dc0510a06ea451e7f84bcb
Thomas Dziedzic <gostrc@gmail.com>
2012-10-15 23:00:14 +0000

    updated on Mon Oct 15 23:00:14 UTC 2012

--- cloudprint-server-systemd/generate_cloudprint_config.sh
+++ cloudprint-server-systemd/generate_cloudprint_config.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
 
-cd '/etc/cloudprint-server/'
+cd '/etc/cloudprint-server/connectors/'
 python2 '/usr/lib/cloudprint-server/generate_cloudprint_config.py'
+
+newf="$(ls -1 -rt | tail -1)"
+cp -v "$newf" '/etc/cloudprint-server/Service State'
