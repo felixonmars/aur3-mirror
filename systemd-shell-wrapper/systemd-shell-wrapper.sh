96d416bdc5f23a775081ff9b65a666967f2565b0
Thomas Dziedzic <gostrc@gmail.com>
2012-10-23 23:00:22 +0000

    updated on Tue Oct 23 23:00:22 UTC 2012

--- systemd-shell-wrapper/systemd-shell-wrapper.sh
+++
@@ -1,9 +0,0 @@
-if [ "$BASH_VERSION" ] && [ -n "$PS1" ] && echo $SHELLOPTS | grep -v posix >>/dev/null; then
-    if [ -f /etc/profile.d/systemd-shell-wrapper.bash ]; then
-        source /etc/profile.d/systemd-shell-wrapper.bash
-    fi
-#elif [ "$ZSH_VERSION" ] && [ -n "$PS1" ]; then
-#    if [ -f /etc/profile.d/ ]; then
-#        source /etc/profile.d/
-#    fi
-fi
