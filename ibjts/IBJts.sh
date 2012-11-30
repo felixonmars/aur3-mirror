f6e3bc9f106f03ce6fe4dd7303296934cdf84d83
Thomas Dziedzic <gostrc@gmail.com>
2012-11-30 00:00:04 +0000

    updated on Fri Nov 30 00:00:03 UTC 2012

--- ibjts/IBJts.sh
+++ ibjts/IBJts.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 test -d $HOME/Jts || mkdir $HOME/Jts
-cd ${0%/*} && java -cp jts.jar:pluginsupport.jar:riskfeed.jar:hsqldb.jar:jcommon-1.0.12.jar:jfreechart-1.0.9.jar:jhall.jar:other.jar:rss.jar -Xmx256M jclient.LoginFrame $HOME/Jts gw4.ibllc.com:4000
+cd ${0%/*} && java -cp jts.jar:hsqldb.jar:jcommon-1.0.12.jar:jfreechart-1.0.9.jar:jhall.jar:other.jar:rss.jar -Xmx512M -XX:MaxPermSize=128M jclient.LoginFrame $HOME/Jts gw4.ibllc.com:4000
