566989dcd01789511319e84c1629a2b7838c42e5
Thomas Dziedzic <gostrc@gmail.com>
2013-01-01 21:00:03 +0000

    updated on Tue Jan  1 21:00:03 UTC 2013

---
+++ craftbukkit-stable/craftbukkit.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+BACKUPPATH="/srv/craftbukkit/backup/"
+WORLDPATH="/srv/craftbukkit/world/"
+
+case "$1" in
+  start)
+    if [ -z "`pgrep -f -n 'craftbukkit.jar'`" ]; then
+      /usr/bin/tmux new-session -d -s craftbukkit-console -d 'cd /srv/craftbukkit; java -Xmx1024M -Xms1024M -jar /srv/craftbukkit/craftbukkit.jar nogui'
+      if [ $? -gt 0 ]; then
+        exit 1
+      fi
+    else
+      echo "Craftbukkit already started"
+      exit 1
+    fi
+    ;;
+
+  stop)
+    tmux send-keys -t craftbukkit-console 'broadcast NOTICE: Server shutting down in 5 seconds!' C-m
+    sleep 5
+    tmux send-keys -t craftbukkit-console 'stop' C-m
+    sleep 10
+    ;;
+
+  backup)
+    FILE="`date +%Y%m%d%H%M`.tar.gz"
+    path="$BACKUPPATH/$FILE"
+    mkdir -p $BACKUPPATH
+    tmux send-keys 'save-off' C-m
+    tmux send-keys 'save-all' C-m
+    tar -czf $path $WORLDPATH
+    tmux send-keys 'save-on' C-m
+    ;;
+
+  console)
+    tmux attach -t craftbukkit-console
+    ;;
+
+  *)
+    echo "usage: $0 {start|backup|console} (need to run as 'craftbukkit')"
+esac
+
+exit 0
+
