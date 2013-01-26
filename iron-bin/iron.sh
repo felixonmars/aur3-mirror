56bfec702a4f4483f29e59148b1de1307df23a08
Thomas Dziedzic <gostrc@gmail.com>
2013-01-26 14:10:44 +0000

    updated on Sat Jan 26 14:10:43 UTC 2013

--- iron-bin/iron.sh
+++ iron-bin/iron.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-LD_LIBRARY_PATH="/usr/lib:/opt/iron" /opt/iron/iron "$@"
+[[ -f '/etc/iron/default' ]] && . '/etc/iron/default'
+
+LD_LIBRARY_PATH="/usr/lib:/opt/iron" /opt/iron/iron ${IRON_FLAGS} "$@"
