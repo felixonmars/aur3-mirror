406df4a9e78edec83e7a0a2c323e34d9b9ea5f4b
Thomas Dziedzic <gostrc@gmail.com>
2013-01-26 08:04:46 +0000

    updated on Sat Jan 26 08:04:46 UTC 2013

--- ncl/ncarg.sh
+++ ncl/ncarg.sh
@@ -4,3 +4,9 @@ export NCARG_DATABASE=/usr/lib/ncarg/database
 export NCARG_FONTCAPS=/usr/lib/ncarg/fontcaps
 export NCARG_GRAPHCAPS=/usr/lib/ncarg/graphcaps
 export NCARG_USRRESFILE=/usr/lib/ncarg/hluresfile
+export NCARG_NCARG=/usr/lib/ncarg
+export NCARG_MAN=/usr/share/man
+
+# if you need modify this file, copy this hluresfile and reset this 
+# variable to the new location in your ~/.bashrc, not modify this file.
+export NCARG_USRRESFILE=/usr/lib/ncarg/hluresfile
