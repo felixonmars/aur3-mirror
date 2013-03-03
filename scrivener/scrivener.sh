0838ebfc7fba0d864ed05524ec772b2df579ae63
Thomas Dziedzic <gostrc@gmail.com>
2013-03-03 22:13:11 +0000

    updated on Sun Mar  3 22:13:09 UTC 2013

--- scrivener/scrivener.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#unsetting this variable seems to be needed to keep from using incompatible Arch QT libs
-unset QT_PLUGIN_PATH
-
-LD_LIBRARY_PATH=/opt/scrivener/lib /opt/scrivener/bin/Scrivener
-EOF
