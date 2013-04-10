d1aa8cfcb217d1b3f45751e1ed44418120b5686d
Thomas Dziedzic <gostrc@gmail.com>
2013-04-10 16:04:47 +0000

    updated on Wed Apr 10 16:04:46 UTC 2013

--- seamonkey-kde/vendor.js
+++ seamonkey-kde/vendor.js
@@ -1,3 +1,21 @@
+// Use LANG environment variable to choose locale
+pref("intl.locale.matchOS", true);
+
+// Disable default browser checking.
+pref("browser.shell.checkDefaultBrowser", false);
+
 // Don't disable our bundled extensions in the application directory
 pref("extensions.autoDisableScopes", 11);
 pref("extensions.shownSelectionUI", true);
+
+// Auto-update is useless
+pref("app.update.enabled", false);
+
+// From kde.js
+pref("browser.preferences.instantApply", false);
+
+pref("browser.showQuitWarning", true);
+pref("browser.download.manager.scanWhenDone", false);
+pref("browser.display.use_system_colors", true);
+pref("general.smoothScroll", true);
+
