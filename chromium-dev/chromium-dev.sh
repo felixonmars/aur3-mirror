0664be1ca9d5a3d176c0546a0f53871e45993127
Thomas Dziedzic <gostrc@gmail.com>
2013-05-02 04:26:33 +0000

    updated on Thu May  2 04:26:30 UTC 2013

--- chromium-dev/chromium-dev.sh
+++ chromium-dev/chromium-dev.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
-if [ -f /etc/chromium-dev/default ] ; then
+
+# Allow users to override command-line options
+# Based on Gentoo's chromium package (and by extension, Debian's)
+if [[ -f /etc/chromium-dev/default ]] ; then
   . /etc/chromium-dev/default
 fi
 
+# Prefer user defined CHROMIUM_USER_FLAGS (from env) over system
+# default CHROMIUM_FLAGS (from /etc/chromium-dev/default)
 CHROMIUM_FLAGS=${CHROMIUM_USER_FLAGS:-"$CHROMIUM_FLAGS"}
 
-export CHROME_WRAPPER=/usr/lib/chromium-dev/chromium
+export CHROME_WRAPPER=$(readlink -f "$0")
 export CHROME_DESKTOP=chromium-dev.desktop
 
-exec /usr/lib/chromium-dev/chromium --class=chromium-dev ${CHROMIUM_FLAGS} "$@"
+exec /usr/lib/chromium-dev/chromium --class=chromium-dev $CHROMIUM_FLAGS "$@"
