0a4d2cba37829471dfb6684e7d46906014da150b
Thomas Dziedzic <gostrc@gmail.com>
2013-05-03 22:20:17 +0000

    updated on Fri May  3 22:20:15 UTC 2013

--- craftbukkit/craftbukkit.sh
+++ craftbukkit/craftbukkit.sh
@@ -9,6 +9,15 @@ if [ -z $2 ]; then
         USER="craftbukkit"
 fi
 
+save_stop() {
+    su -s /bin/bash -c "tmux send-keys 'save-off' C-m" ${USER}
+    su -s /bin/bash -c "tmux send-keys 'save-all' C-m" ${USER}
+}
+
+save_start() {
+    su -s /bin/bash -c "tmux send-keys 'save-on' C-m" ${USER}
+}
+
 case "$1" in
   start)
     if [ -z "`pgrep -f -n 'craftbukkit.jar'`" ]; then
@@ -31,6 +40,17 @@ case "$1" in
 
   console)
     sudo -u ${USER} tmux attach -t craftbukkit-console
+    ;;
+  
+  backup)
+    stat_busy "Starting backup"
+    FILE="`date +%Y%m%d%H%M`.tar.gz"
+    path="$BACKUPPATH/$FILE"
+    su -s /bin/bash -c "mkdir -p $BACKUPPATH" ${USER}
+    save_stop
+    su -s /bin/bash -c "tar -czf $path $WORLDPATH" ${USER}
+    save_start
+    stat_done "Backup finished"
     ;;
 
   *)
