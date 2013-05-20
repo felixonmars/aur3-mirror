c260ccda764b74d1a5e00c147a998ad8c720263b
Thomas Dziedzic <gostrc@gmail.com>
2013-05-20 00:09:53 +0000

    updated on Mon May 20 00:09:51 UTC 2013

--- freearc/freearcusercfg.sh
+++ freearc/freearcusercfg.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ ! -d "$HOME/.FreeArc" ]; then
- 	mkdir -p $HOME/.FreeArc
-	cp /etc/FreeArc/* $HOME/.FreeArc
-	cp /usr/lib/FreeArc/scripts/*.lua $HOME/.FreeArc
-	sudo chown $USER:$USER $HOME/.FreeArc/*
+if [[ ! -d "$HOME/.FreeArc" ]]; then
+    install -d $HOME/.FreeArc
+    cp /etc/FreeArc/* $HOME/.FreeArc
+    cp /usr/lib/FreeArc/scripts/*.lua $HOME/.FreeArc
+    sudo chown $USER:$USER $HOME/.FreeArc/*
 fi
