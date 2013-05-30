eeb7ef0cfaa5daa3b3e51f74ff103750f65ea4f6
Thomas Dziedzic <gostrc@gmail.com>
2013-05-30 08:00:14 +0000

    updated on Thu May 30 08:00:13 UTC 2013

---
+++ sphinx-svn/sphinx.rc.d
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+. /etc/rc.conf
+. /etc/rc.d/functions
+. /etc/conf.d/sphinx
+
+PID=$(pidof -o %PPID /usr/bin/sphinx-searchd)
+case $1 in
+start)
+        stat_busy "Starting Sphinx Daemon"
+
+        if [[ -z $PID ]] && /usr/bin/sphinx-searchd $SPHINX_ARGS; then
+
+                PID=$(pidof -o %PPID /var/run/sphinx/searchd.pid)
+                echo "$PID" > /var/run/sphinx/searchd.pid
+                add_daemon sphinx
+                stat_done
+        else
+                stat_fail
+                exit 1
+        fi
+        ;;
+
+stop)
+        stat_busy "Stopping Sphinx Daemon"
+        if [[ ! -z $PID ]]  && kill "$PID" &>/dev/null; then
+                rm_daemon sphinx
+                stat_done
+        else
+                stat_fail
+                exit 1
+        fi
+        ;;
+
+restart)
+        $0 stop
+        $0 start
+        ;;
+
+*)
+        echo "Usage: $0 {start|stop|restart}" >&2
+        exit 1
+esac
