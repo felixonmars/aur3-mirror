82a90b80b43a6678b90671756fee31e15df33627
Thomas Dziedzic <gostrc@gmail.com>
2013-06-03 06:00:10 +0000

    updated on Mon Jun  3 06:00:10 UTC 2013

---
+++ ejre/ejre.sh
@@ -0,0 +1,2 @@
+export PATH=${PATH}:/opt/java-embedded/jre/bin
+export JAVA_HOME=${JAVA_HOME:-/opt/java-embedded/jre}
--- ejre/jre.sh
+++
@@ -1,3 +0,0 @@
-export J2REDIR=/opt/ejre/jre
-export PATH=${PATH}:/opt/ejre/jre/bin
-export JAVA_HOME=${JAVA_HOME:-/opt/ejre/jre}
