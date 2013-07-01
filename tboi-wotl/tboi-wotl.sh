70e976face67ef98e07b46bc896ff95225ebf5b2
Thomas Dziedzic <gostrc@gmail.com>
2013-07-01 00:08:10 +0000

    updated on Mon Jul  1 00:08:01 UTC 2013

--- tboi-wotl/tboi-wotl.sh
+++ tboi-wotl/tboi-wotl.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-exec $(xdg-settings get default-web-browser) /usr/share/tboi-wotl/tboi-wotl.swf
+_desktopfile="$(locate $(xdg-settings get default-web-browser) | tail -n 1)"
+_swf="/usr/share/tboi-wotl/tboi-wotl.swf"
+
+exec $(grep "^Exec=" $_desktopfile | head -n 1 | sed "s/Exec=//" | sed "s:%[Uu]:$_swf:")
