b564bc9b784ac04185d658fd5653c6d27286f8e2
Thomas Dziedzic <gostrc@gmail.com>
2013-09-05 14:00:23 +0000

    updated on Thu Sep  5 14:00:23 UTC 2013

--- wildfly/systemd-wrapper.sh
+++ wildfly/systemd-wrapper.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source /etc/profile.d/wildfly.sh
 source /etc/profile.d/jdk.sh
-exec /usr/share/wildfly/bin/$JBOSS_AS_MODE.sh "$@"
+
+exec /usr/share/wildfly/bin/$WILDFLY_MODE.sh "$@"
 
--- wildfly/wildfly.sh
+++ wildfly/wildfly.sh
@@ -1,13 +1,13 @@
-# Wildfly's Home
-export JBOSS_HOME=/usr/share/wildfly
+# Application server mode
+export WILDFLY_MODE=standalone
 
-# Mode
-export JBOSS_AS_MODE=standalone
+# Home of Wildfly AS
+export JBOSS_HOME=/usr/share/wildfly
 
 # Configuration file
 export JBOSS_CONFIG=standalone.xml
 
-# Wildfly user
+# AS user
 export JBOSS_USER=wildfly
 
 # PID file
@@ -18,3 +18,4 @@ export JBOSS_CONSOLE_LOG=/var/log/wildfly/console.log
 
 # Run in background
 export LAUNCH_JBOSS_IN_BACKGROUND=1
+
