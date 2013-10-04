7c440b2efdc03b8e2b84dd0b155fb30895564f2c
Thomas Dziedzic <gostrc@gmail.com>
2013-10-04 16:27:06 +0000

    updated on Fri Oct  4 16:27:02 UTC 2013

--- xmonad-gnome3/xmonad-sample.hs
+++ xmonad-gnome3/xmonad-sample.hs
@@ -6,6 +6,7 @@ import XMonad.Util.EZConfig
 myConfig = defaultConfig { modMask = mod4Mask, terminal = "xfce4-terminal" }
     `additionalKeysP`
         [ ("M-f", spawn "nautilus --no-desktop ~/Downloads")
+        , ("M-S-q", spawn "gnome-session-quit --no-prompt")
         , ("M-S-,", spawn "gnome-control-center") ]
 
 main = xmonad =<< xmobar myConfig
